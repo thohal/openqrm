cddbeaf31ee9685957a83739f4cfa083b9554cba
akuballa <akuballa@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2008-02-26 15:02:53 +0000

    New framework
    git-svn-id: http://openqrm.svn.sourceforge.net/svnroot/openqrm@61 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

--- trunk/src/web/base/index.php
+++ trunk/src/web/base/index.php
@@ -1,13 +1,22 @@
-<?php
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN"
+   "http://www.w3.org/TR/html4/frameset.dtd">
+<html>
+<head>
+<title>Beschreibung des Frameset-Inhalts</title>
+</head>
+<frameset cols="200,*">
+<frame src="menu.php" name="NaviFrame" frameborder="0">
+  <frameset rows="100,*">
+  <frame src="top.php" name="TopFrame" frameborder="0" noresize scrolling="no">
+  <frame src="http://www.mozilla.org/" name="MainFrame" frameborder="0">
+  </frameset>
 
-echo "Main"
-
-
-?>
-<br>
-<br>
-<a href="resource-overview.php">Resource overview</a>
-<br>
-
-
-
+  <noframes>
+    <body>
+      <h1>Alternativ-Inhalt</h1>
+      <p>Wird angezeigt, wenn der Browser keine Frames darstellen kann.</p>
+      <p>Mindest-Inhalt sollten eine Kurzbeschreibung und eine Sitemap sein.</p>
+    </body>
+  </noframes>
+</frameset>
+</html>
