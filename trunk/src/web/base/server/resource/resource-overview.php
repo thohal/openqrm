ebf94439ff63bc2102141f7ee929a9fab8fc3f35
mattr_sf <mattr_sf@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2008-02-29 09:18:03 +0000

    recommit overwritten changes
    git-svn-id: http://openqrm.svn.sourceforge.net/svnroot/openqrm@96 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

--- trunk/src/web/base/server/resource/resource-overview.php
+++ trunk/src/web/base/server/resource/resource-overview.php
@@ -1,13 +1,11 @@
-
-<link rel="stylesheet" type="text/css" href="../../css/htmlobject.css" />
-
 <?php
 
 $RootDir = $_SERVER["DOCUMENT_ROOT"].'openqrm/base/';
 require_once "$RootDir/class/resource.class.php";
 // using the htmlobject class
-require_once "$RootDir/include/htmlobject.inc.php";
-
+require_once "$RootDir/class/htmlobject_box.class.php";
+require_once "$RootDir/class/htmlobject_select.class.php";
+require_once "$RootDir/class/htmlobject_textarea.class.php";
 
 echo "<b>Resource overview</b>";
 echo "<br>";
@@ -26,11 +24,10 @@ $OPENQRM_RESOURCE_COUNT_OFFLINE = $resource_tmp->get_count("offline");
 echo "Offline resources: $OPENQRM_RESOURCE_COUNT_OFFLINE";
 echo "<br>";
 
-
+/*
 
 echo "adding resource ...";
 $resource_tmp->add("1", "00:13:8F:0D:BB:B1", "10.20.30.40");
-/*
 echo "<br>";
 echo "<br>";
 $OPENQRM_RESOURCE_COUNT_ALL=$resource_tmp->get_count("all");
@@ -56,26 +53,29 @@ $resource_tmp->remove(1, "00:13:8F:0D:BB:B1");
 			$resource = new resource();
 			$resource->get_instance_by_id($resource_db["resource_id"]);
 			if ("$resource->id" != "0") {
-				echo "<div id=\"resource\" nowrap=\"true\">";
 				echo "<form action='../../../action/resource-action.php' method=post>";
-				echo "$resource->id ";
+				echo "resource&nbsp;&nbsp; $resource->id &nbsp;";
 				// local or netboot
 				if ("$resource->localboot" == "0") {
-					echo "<a href=\"../../../action/resource-action.php?resource_command=localboot&resource_id=$resource->id&resource_ip=$resource->ip&resource_mac=$resource->mac\"> net</a>";
+					echo "<a href=\"../../../action/resource-action.php?resource_command=localboot&resource_id=$resource->id&resource_ip=$resource->ip&resource_mac=$resource->mac\">net</a>";
 				} else {
-					echo "<a href=\"../../../action/resource-action.php?resource_command=netboot&resource_id=$resource->id&resource_ip=$resource->ip&resource_mac=$resource->mac\"> local</a>";
+					echo "<a href=\"../../../action/resource-action.php?resource_command=netboot&resource_id=$resource->id&resource_ip=$resource->ip&resource_mac=$resource->mac\">local</a>";
 				}
-				echo " $resource->kernel $resource->kernelid $resource->image $resource->imageid $resource->ip $resource->mac $resource->hostname $resource->state ";
+				echo "&nbsp; $resource->kernel &nbsp; $resource->kernelid &nbsp; $resource->image &nbsp; $resource->imageid &nbsp; $resource->ip &nbsp; $resource->mac &nbsp; $resource->hostname &nbsp; $resource->state &nbsp; $resource->event &nbsp; ";
 
-				$resource_action_ar = array();
-				$resource_action_ar[] = array("value"=>'', "label"=>'',);
-				$resource_action_ar[] = array("value"=>'reboot', "label"=>'reboot',);
-				$resource_action_ar[] = array("value"=>'halt', "label"=>'halt',);
-				$resource_action_ar[] = array("value"=>'remove', "label"=>'remove',);
-				$resource_action_selected_ar[] = array("value"=>'', "label"=>'',);
-
-				$select = htmlobject_select('resource_command', $resource_action_ar, '', $resource_action_selected_ar);
-				echo $select;
+				$select = new htmlobject_select();
+				$select->id = 'id';
+				$select->name = 'resource_command';
+				$select->css = 'select';
+				$select->tabindex = 1;
+				$select->title = 'Resource-Actions';
+				$select->size = 2;
+				$select->style = 'white-space:nowrap;';
+				$select->multiple = false;
+				$select->disabled = false;
+				$select->text = array('','reboot','halt','remove');
+				$select->selected = array('');
+				echo $select->get_string();
 
 				echo "<input type=hidden name=resource_ip value=$resource->ip>";
 				echo "<input type=hidden name=resource_id value=$resource->id>";
@@ -89,13 +89,10 @@ $resource_tmp->remove(1, "00:13:8F:0D:BB:B1");
 
 				echo "<input type=submit value='apply'>";
 				echo "</form>";
-				echo "</div>";
 
 			} else {
 				echo "<br>";
-				echo "<div id=\"resource\" nowrap=\"true\">";
 				echo "openQRM $resource->id &nbsp; $resource->localboot";
-				echo "</div>";
 				echo "<br>";
 			}
 
