f47869904ab08455ed9f3e651e5122fa5c3aaedd
mattr_sf <mattr_sf@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2008-03-04 11:01:03 +0000

    added user authentification for plugin-actions
    git-svn-id: http://openqrm.svn.sourceforge.net/svnroot/openqrm@138 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

--- trunk/src/web/base/plugins/aa_plugins/plugin-action.php
+++ trunk/src/web/base/plugins/aa_plugins/plugin-action.php
@@ -7,10 +7,16 @@
 
 <?php
 
-
+require_once('../../include/user.inc.php');
 $RootDir = $_SERVER["DOCUMENT_ROOT"].'openqrm/base/';
 require_once "$RootDir/class/plugin.class.php";
 require_once "$RootDir/class/openqrm_server.class.php";
+
+$user = new user($_SERVER['PHP_AUTH_USER']);
+$user->set_user();
+if ($user->role != "administrator") {
+	exit();
+}
 
 $plugin_command = $_REQUEST["plugin_command"];
 $plugin_name = $_REQUEST["plugin_name"];
