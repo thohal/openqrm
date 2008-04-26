5f2b84345ac036cdac69e1fdd9c45a878a6405aa
mattr_sf <mattr_sf@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2008-04-26 21:44:18 +0000

    more plugin docu
    git-svn-id: http://openqrm.svn.sourceforge.net/svnroot/openqrm@392 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

--- trunk/src/plugins/dhcpd/web/dhcpd-about.php
+++ trunk/src/plugins/dhcpd/web/dhcpd-about.php
@@ -16,7 +16,7 @@ require_once "$RootDir/include/htmlobject.inc.php";
 
 function dhcpd_about() {
 
-	$disp = "<h1>Dhcpd plugin</h1>";
+	$disp = "<h1><img border=0 src=\"/openqrm/base/plugins/dhcpd/img/plugin.png\"> Dhcpd plugin</h1>";
 	$disp = $disp."<br>";
 	$disp = $disp."The dhcpd-plugin automatically manages your ip-address assignment and network-boot environemnt for the rapid-deployment features of openQRM.";
 	$disp = $disp." Since the dynamic deployment methods in openQRM are based on network-booting (PXE) a dhcpd-server is a fundamental service to assign ip-addresses to booting resources.";
--- trunk/src/plugins/iscsi-storage/web/iscsi-storage-about.php
+++ trunk/src/plugins/iscsi-storage/web/iscsi-storage-about.php
@@ -14,58 +14,67 @@ $RootDir = $_SERVER["DOCUMENT_ROOT"].'openqrm/base/';
 require_once "$RootDir/include/user.inc.php";
 require_once "$RootDir/include/htmlobject.inc.php";
 
-function aoe_about() {
+function iscsi_about() {
 
-	$disp = "<h1><img border=0 src=\"/openqrm/base/plugins/aoe-storage/img/plugin.png\"> Aoe-storage plugin</h1>";
+	$disp = "<h1><img border=0 src=\"/openqrm/base/plugins/iscsi-storage/img/plugin.png\"> Iscsi-storage plugin</h1>";
 	$disp = $disp."<br>";
-	$disp = $disp."The Aoe-storage plugin integrates Aoe/Coraid Storage into openQRM.";
-	$disp = $disp." It adds a new storage-type 'aoe-storage' and a new deployment-type 'aoe-root' to";
+	$disp = $disp."The Iscsi-storage plugin integrates Iscsi-Target Storage into openQRM.";
+	$disp = $disp." It adds a new storage-type 'iscsi-storage' and a new deployment-type 'iscsi-root' to";
 	$disp = $disp." the openQRM-server during initialization. ";
 	$disp = $disp."<br>";
 	$disp = $disp."<br>";
-	$disp = $disp."Aoe-storage type :";
+	$disp = $disp."Iscsi-storage type :";
 	$disp = $disp."A linux-box (resource) with 'vblade' installed should be used to create";
-	$disp = $disp." a new Storage-server through the openQRM-GUI. The Aoe-storage system can be either";
+	$disp = $disp." a new Storage-server through the openQRM-GUI. The Iscsi-storage system can be either";
 	$disp = $disp." deployed via openQRM or integrated into openQRM within the 'local-server' plugin.";
-	$disp = $disp."openQRM then automatically manages the vblade disks on the Aoe-storage server.";
+	$disp = $disp."openQRM then automatically manages the vblade disks on the Iscsi-storage server.";
 	$disp = $disp."<br>";
 	$disp = $disp."<br>";
-	$disp = $disp."Aoe-deployment type :";
+	$disp = $disp."Iscsi-deployment type :";
 	$disp = $disp."<br>";
-	$disp = $disp."The Aoe-deployment type supports to boot servers/resources from the Aoe-stoage server.";
-	$disp = $disp." Server images created with the 'aoe-root' deployment type are stored on Storage-server";
-	$disp = $disp." from the storage-server type 'aoe-storage'. During startup of an appliance they are directly";
-	$disp = $disp." attached to the resource as its rootfs via the aoe-protokol.";
+	$disp = $disp."The Iscsi-deployment type supports to boot servers/resources from the Iscsi-stoage server.";
+	$disp = $disp." Server images created with the 'iscsi-root' deployment type are stored on Storage-server";
+	$disp = $disp." from the storage-server type 'iscsi-storage'. During startup of an appliance they are directly";
+	$disp = $disp." attached to the resource as its rootfs via the iscsi-protokol.";
 	$disp = $disp."<br>";
 	$disp = $disp."<br>";
 	$disp = $disp."How to use :";
 	$disp = $disp."<br>";
-	$disp = $disp."- Create an Aoe-storage server via the 'Storage-Admin' (Storage menu)";
+	$disp = $disp."- Create an Iscsi-storage server via the 'Storage-Admin' (Storage menu)";
 	$disp = $disp."<br>";
-	$disp = $disp."- Create a Disk-shelf on the Aoe-storage using the 'Shelfs' link (Aoe-plugin menu)";
+	$disp = $disp."- Create a Disk-shelf on the Iscsi-storage using the 'Luns' link (Iscsi-plugin menu)";
 	$disp = $disp."<br>";
-	$disp = $disp."- Create an (Aoe-) Image ('Add Image' in the Immage-overview).";
-	$disp = $disp." Then select the Aoe-storage server and give the Aoe-device name as the images root-device.";
+	$disp = $disp."- Create an (Iscsi-) Image ('Add Image' in the Immage-overview).";
+	$disp = $disp." Then select the Iscsi-storage server and give the Iscsi-device name as the images root-device (e.g. /dev/sda1).";
+	$disp = $disp." Add the image-parameter 'IMAGE_TARGET' to the image-capabilities fields set to the Iscsi-target name the image is located.";
 	$disp = $disp." Eventually add addtional optional Image-parameter in the 'image-capabilites' field.";
 	$disp = $disp."<br>";
-	$disp = $disp."- Create an Appliance using one of the available kernel and the Aoe-Image created in the previous steps.";
+	$disp = $disp."- Create an Appliance using one of the available kernel and the Iscsi-Image created in the previous steps.";
 	$disp = $disp."<br>";
 	$disp = $disp."- Start the Appliance";
 	$disp = $disp."<br>";
 	$disp = $disp."<br>";
-	$disp = $disp."Additional Aoe image-parameters :";
+	$disp = $disp."Required Iscsi image-parameters :";
+	$disp = $disp."<br>";
+	$disp = $disp."IMAGE_TARGET=[Iscsi-target-name]";
+	$disp = $disp."<br>";
+	$disp = $disp."Should be set to the target name of the Iscsi-target server where the image is located";
+	$disp = $disp." deployment time.";
+	$disp = $disp."<br>";
+	$disp = $disp."<br>";
+	$disp = $disp."Additional Iscsi image-parameters :";
 	$disp = $disp."<br>";
 	$disp = $disp."IMAGE_INSTALL_FROM=[nfs-storage:path-to-existing-nfs-image]";
 	$disp = $disp."<br>";
 	$disp = $disp."This parameter can be set to an (nfs) location from which the image will be installed at";
 	$disp = $disp." deployment time.";
 	$disp = $disp."<br>";
-	$disp = $disp."The syntax is : ip_of_aoe-server:path_to_target_image";
+	$disp = $disp."The syntax is : ip_of_iscsi-server:path_to_target_image";
 	$disp = $disp."<br>";
 	$disp = $disp."<br>";
 	$disp = $disp."IMAGE_INSTALL_FROM_LOCAL_DEVICE=[/dev/hdX|/dev/sdX]";
 	$disp = $disp."<br>";
-	$disp = $disp."Set to a local harddisk device (e.g. /dev/hda1) this option will install the aoe-storage image on";
+	$disp = $disp."Set to a local harddisk device (e.g. /dev/hda1) this option will install the iscsi-storage image on";
 	$disp = $disp." boot-time from the local-device configured in the image-parameters";
 	$disp = $disp."<br>";
 	$disp = $disp."<br>";
@@ -77,7 +86,7 @@ function aoe_about() {
 	$disp = $disp."IMAGE_TRANSFORM_TO_LOCAL_DEVICE=[/dev/hdX|/dev/sdX]";
 	$disp = $disp."<br>";
 	$disp = $disp."If this parameter is set to a local harddisk device (e.g. /dev/hda1) this option will transfrom";
-	$disp = $disp." the aoe-storage image on boot-time to the local-device configured in the image-parameters";
+	$disp = $disp." the iscsi-storage image on boot-time to the local-device configured in the image-parameters";
 	$disp = $disp."<br>";
 	$disp = $disp."<br>";
 	$disp = $disp."IMAGE_TRANSFORM_TO_LOCAL_DEVICE_FS_TYPE";
@@ -89,7 +98,7 @@ function aoe_about() {
 
 
 $output = array();
-$output[] = array('label' => 'About', 'value' => aoe_about());
+$output[] = array('label' => 'About', 'value' => iscsi_about());
 echo htmlobject_tabmenu($output);
 
 ?>
