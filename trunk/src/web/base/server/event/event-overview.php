13693387d03e0cb7f4321f823c0bf24cc55d2278
akuballa <akuballa@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2008-04-27 10:06:52 +0000

    new events page
    git-svn-id: http://openqrm.svn.sourceforge.net/svnroot/openqrm@396 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

--- trunk/src/web/base/server/event/event-overview.php
+++ trunk/src/web/base/server/event/event-overview.php
@@ -1,143 +1,96 @@
-
 <link rel="stylesheet" type="text/css" href="../../css/htmlobject.css" />
-<style>
-.htmlobject_tab_box {
-	width:700px;
-}
-</style>
+<link rel="stylesheet" type="text/css" href="event.css" />
 
 <?php
+$thisfile = basename($_SERVER['PHP_SELF']);
 
 $RootDir = $_SERVER["DOCUMENT_ROOT"].'/openqrm/base/';
 require_once "$RootDir/include/user.inc.php";
 require_once "$RootDir/class/event.class.php";
 require_once "$RootDir/include/htmlobject.inc.php";
 
+function event_display() {
+global $OPENQRM_USER;
+global $thisfile;
 
-function event_display($admin) {
 	$event_tmp = new event();
-	$OPENQRM_EVENT_COUNT_ALL = $event_tmp->get_count();
+	$table = new htmlobject_db_table('event_priority');
 
-	if ("$admin" == "admin") {
-		$disp = "<h1>Event Admin</h1>";
-	} else {
-		$disp = "<h1>Event List</h1>";
-	}
-	$disp = $disp."<br>";
-	$disp .= "<br>";
-	$disp = $disp."<div id=\"all_event\" nowrap=\"true\">";
-	$disp = $disp."All events: $OPENQRM_EVENT_COUNT_ALL";
-	$disp = $disp."</div>";
-	$disp = $disp."<br>";
+	$disp = '<h1>Event List</h1>';
 
-	$disp = $disp."<hr>";
+	$arHead = array();
+	$arHead['event_priority'] = array();
+	$arHead['event_priority']['title'] ='Status';
 
-	$disp .= "<table>";
-	$disp .= "<tr><td>";
-	$disp .= "";
-	$disp .= "</td><td>";
-	$disp .= "id";
-	$disp .= "</td><td>";
-	$disp .= "time";
-	$disp .= "</td><td>";
-	$disp .= "source";
-	$disp .= "</td><td>";
-	$disp .= "description";
-	$disp .= "</td><td>";
-	if ("$admin" == "admin") {
-		$disp .= "action";
-	}
-	$disp .= "</td><td>";
-	$disp .= "</td></tr>";
+	$arHead['event_id'] = array();
+	$arHead['event_id']['title'] ='ID';
 
-	$event_array = $event_tmp->display_overview(0, 25);
+	$arHead['event_time'] = array();
+	$arHead['event_time']['title'] ='Time';
+
+	$arHead['event_source'] = array();
+	$arHead['event_source']['title'] ='Source';
+
+	$arHead['event_description'] = array();
+	$arHead['event_description']['title'] ='Description';
+	$arHead['event_description']['sortable'] = false;
+
+	$arBody = array();
+	$event_array = $event_tmp->display_overview($table->offset, $table->limit, $table->sort, $table->order);
+
+
 	foreach ($event_array as $index => $event_db) {
 		$event = new event();
 		$event->get_instance_by_id($event_db["event_id"]);
-		$disp .= "</td><td>";
-		$prio_icon="/openqrm/base/img/transition.png";
+		$prio_icon="transition.png";
 		switch ($event->priority) {
-			case 0:
-				$prio_icon="/openqrm/base/img/off.png";
-				break;
-			case 1:
-				$prio_icon="/openqrm/base/img/error.png";
-				break;
-			case 2:
-				$prio_icon="/openqrm/base/img/error.png";
-				break;
-			case 3:
-				$prio_icon="/openqrm/base/img/error.png";
-				break;
-			case 4:
-				$prio_icon="/openqrm/base/img/transition.png";
-				break;
-			case 5:
-				$prio_icon="/openqrm/base/img/active.png";
-				break;
-			case 6:
-				$prio_icon="/openqrm/base/img/idle.png";
-				break;
-			case 7:
-				$prio_icon="/openqrm/base/img/idle.png";
-				break;
+			case 0: $prio_icon = "off.png"; 	break;
+			case 1: $prio_icon = "error.png";	break;
+			case 2: $prio_icon = "error.png";	break;
+			case 3:	$prio_icon = "error.png";	break;
+			case 4:	$prio_icon = "transition.png"; 	break;
+			case 5:	$prio_icon = "active.png"; 	break;
+			case 6:	$prio_icon = "idle.png"; 	break;
+			case 7:	$prio_icon = "idle.png"; 	break;
 		}
 		// acknowledged ?
 		if ($event->status == 1) {
-			$prio_icon="/openqrm/base/img/idle.png";
+			$prio_icon="idle.png";
 		}
-		$disp .= "<img src=\"$prio_icon\">";
-		$disp .= "</td><td>";
-		$disp .= "$event->id";
-		$disp .= "</td><td>";
-		$disp = $disp."<div id=\"event\" nowrap=\"true\">";
-		$disp .= date('d F Y h:i:s', $event->time);
-		$disp .= "</td><td>";
-		$disp = $disp."$event->source";
-		$disp .= "</td><td>";
-		$disp = $disp."$event->description";
-		$disp .= "</td><td>";
-		$disp = $disp."<form action='event-action.php' method=post>";
-		$disp = $disp."<input type=hidden name=event_id value=$event->id>";
-		$disp = $disp."<input type=hidden name=event_name value=$event->name>";
-		$disp = $disp."<input type=hidden name=event_command value='ack'";
-		if ("$admin" == "admin") {
-			$disp = $disp."<input type=submit value='Ack'>";
-		}
-		$disp = $disp."</form>";
+		$arBody[] = array(
+			'event_priority' => '<img src="/openqrm/base/img/'.$prio_icon.'">',
+			'event_id' => $event_db["event_id"],
+			'event_time' => date('d F Y h:i:s', $event->time),
+			'event_source' => $event->source,
+			'event_description' => $event->description,
+		);
 
-		$disp .= "</td><td>";
-		$disp = $disp."<form action='event-action.php' method=post>";
-		$disp = $disp."<input type=hidden name=event_id value=$event->id>";
-		$disp = $disp."<input type=hidden name=event_name value=$event->name>";
-		$disp = $disp."<input type=hidden name=event_command value='remove'";
-		if ("$admin" == "admin") {
-			$disp = $disp."<input type=submit value='Remove'>";
-		}
-		$disp = $disp."</form>";
-
-		$disp = $disp."</div>";
-		$disp .= "</td></tr>";
 	}
 
-	$disp .= "</table>";
-	$disp = $disp."<hr>";
-
-	return $disp;
+	$table = new htmlobject_db_table();
+	$table->id = 'Tabelle';
+	$table->css = 'htmlobject_table';
+	$table->border = 1;
+	$table->cellspacing = 0;
+	$table->cellpadding = 3;
+	$table->form_action = $thisfile;
+	$table->head = $arHead;
+	$table->body = $arBody;
+	if ($OPENQRM_USER->role == "administrator") {
+		$table->bottom = array('delete');
+		$table->identifier = 'event_id';
+	}
+	$table->max = $event_tmp->get_count();
+	$table->lang_label_sort = 'sortiere nach';
+	$table->lang_button_refresh = 'aktualisieren';
+	#$table->limit = 10;
+	
+	return $disp.$table->get_string();
 }
-
-
 
 
 $output = array();
-// all user
 $output[] = array('label' => 'Event-List', 'value' => event_display(""));
-// if admin
-if (strstr($OPENQRM_USER->role, "administrator")) {
-	$output[] = array('label' => 'Event-Admin', 'value' => event_display("admin"));
-}
-
 echo htmlobject_tabmenu($output);
 
 ?>
-
