06971db0f62d64092ede987009b496d62df25c5f
mattr_sf <mattr_sf@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2008-05-05 16:43:28 +0000

    made the sshterminal nicer
    git-svn-id: http://openqrm.svn.sourceforge.net/svnroot/openqrm@487 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

--- trunk/src/plugins/sshterm/web/sshterm-manager.php
+++ trunk/src/plugins/sshterm/web/sshterm-manager.php
@@ -2,82 +2,112 @@
 <link rel="stylesheet" type="text/css" href="../../css/htmlobject.css" />
 
 <?php
-
+$thisfile = basename($_SERVER['PHP_SELF']);
 $RootDir = $_SERVER["DOCUMENT_ROOT"].'/openqrm/base/';
 require_once "$RootDir/include/user.inc.php";
 require_once "$RootDir/class/resource.class.php";
 require_once "$RootDir/include/htmlobject.inc.php";
 
+function sshterm_login($id, $ip) {
+	$redirect_url="http://$ip/ajaxterm/";
+	if ("$id" == 0) {
+		$redirect_url="http://localhost:8022";
+	}
+	$left=50+($id*50);
+	$top=100+($id*50);
+?>
+<script type="text/javascript">
+function open_sshterm (url) {
+  sshterm_window = window.open(url, "<?php echo $ip; ?>", "width=580,height=420,left=<?php echo $left; ?>,top=<?php echo $top; ?>");
+  open_sshterm.focus();
+}
+open_sshterm("<?php echo $redirect_url; ?>");
+</script>
+<?php
 
-function sshterm_display($admin) {
-	global $RootDir;
-	$active_resources=0;
-	$resource_icon_default="/openqrm/base/img/resource.png";
+}
+
+function sshterm_display() {
+	global $OPENQRM_USER;
+	global $thisfile;
 
 	$resource_tmp = new resource();
-	$disp = "<h1>SshTerm Manager</h1>";
+	$table = new htmlobject_db_table('resource_id');
 
-	$disp = $disp."<hr>";
+	$disp = '<h1>Resource List</h1>';
+	$disp .= '<br>';
 
-	$disp .= "<table>";
-	$disp .= "<tr><td>";
-	$disp .= "";
-	$disp .= "</td><td>";
-	$disp .= "";
-	$disp .= "</td><td>";
-	$disp .= "id";
-	$disp .= "</td><td>";
-	$disp .= "hostname";
-	$disp .= "</td><td>";
-	$disp .= "</td></tr>";
+	$arHead = array();
+	$arHead['resource_state'] = array();
+	$arHead['resource_state']['title'] ='';
 
-	$resource_array = $resource_tmp->display_overview(0, 10, 'resource_id', 'ASC');
+	$arHead['resource_icon'] = array();
+	$arHead['resource_icon']['title'] ='';
+
+	$arHead['resource_id'] = array();
+	$arHead['resource_id']['title'] ='ID';
+
+	$arHead['resource_hostname'] = array();
+	$arHead['resource_hostname']['title'] ='Name';
+
+	$arHead['resource_ip'] = array();
+	$arHead['resource_ip']['title'] ='Ip';
+
+	$arBody = array();
+	$resource_array = $resource_tmp->display_overview($table->offset, $table->limit, $table->sort, $table->order);
+
 	foreach ($resource_array as $index => $resource_db) {
+		// prepare the values for the array
 		$resource = new resource();
 		$resource->get_instance_by_id($resource_db["resource_id"]);
-		if (("$resource->id" != "0") && ("$resource->state" == "active")) {
-			$active_resources++;
-			$disp = $disp."<div id=\"resource\" nowrap=\"true\">";
-			$disp .= "<tr><td>";
-			$state_icon="/openqrm/base/img/$resource->state.png";
-			// idle ?
-			if (("$resource->imageid" == "1") && ("$resource->state" == "active")) {
-				$state_icon="/openqrm/base/img/idle.png";
-			}
-			if (!file_exists($_SERVER["DOCUMENT_ROOT"].$state_icon)) {
-				$state_icon="/openqrm/base/img/unknown.png";
-			}
-			$disp .= "<img src=\"$state_icon\">";
-			$disp .= "</td><td>";
-			$disp .= "<img src=\"$resource_icon_default\">";
-			$disp .= "</td><td>";
-			$disp = $disp."$resource->id";
-			$disp .= "</td><td>";
-			if (strlen($resource->hostname)) {
-				$disp .= "$resource->hostname";
-			} else {
-				$disp .= "none";
-			}
-
-			$disp .= "</td><td>";
-			$disp .= "<a href=\"http://$resource->ip:8022\"> Login </a>";
-			$disp = $disp."</div>";
-			$disp .= "</td></tr>";
+		$mem_total = $resource_db['resource_memtotal'];
+		$mem_used = $resource_db['resource_memused'];
+		$mem = "$mem_used/$mem_total";
+		$swap_total = $resource_db['resource_swaptotal'];
+		$swap_used = $resource_db['resource_swapused'];
+		$swap = "$swap_used/$swap_total";
+		if ($resource->id == 0) {
+			$resource_icon_default="/openqrm/base/img/logo.png";
+		} else {
+			$resource_icon_default="/openqrm/base/img/resource.png";
 		}
+		$state_icon="/openqrm/base/img/$resource->state.png";
+		// idle ?
+		if (("$resource->imageid" == "1") && ("$resource->state" == "active")) {
+			$state_icon="/openqrm/base/img/idle.png";
+		}
+		if (!file_exists($_SERVER["DOCUMENT_ROOT"].$state_icon)) {
+			$state_icon="/openqrm/base/img/unknown.png";
+		}
+
+		$arBody[] = array(
+			'resource_state' => "<img src=$state_icon>",
+			'resource_icon' => "<img width=24 height=24 src=$resource_icon_default>",
+			'resource_id' => $resource_db["resource_id"],
+			'resource_hostname' => $resource_db["resource_hostname"],
+			'resource_ip' => $resource_db["resource_ip"],
+		);
+
 	}
-	
-	if ($active_resources == 0) {
-			$disp .= "</td><td colspan=5>";
-			$disp = $disp."<div id=\"resource\" nowrap=\"true\">";
-			$disp = $disp."No active resources available for login";
-			$disp = $disp."</div>";
-			$disp .= "</td></tr>";
+
+	$table->id = 'Tabelle';
+	$table->css = 'htmlobject_table';
+	$table->border = 1;
+	$table->cellspacing = 0;
+	$table->cellpadding = 3;
+	$table->form_action = $thisfile;
+	$table->head = $arHead;
+	$table->body = $arBody;
+	if ($OPENQRM_USER->role == "administrator") {
+		$table->bottom = array('login');
+		$table->identifier = 'resource_id';
 	}
+	$table->max = $resource_tmp->get_count('all');
+	#$table->limit = 10;
 	
-	$disp .= "</table>";
-	$disp = $disp."<hr>";
-	return $disp;
+	return $disp.$table->get_string();
 }
+
 
 
 
@@ -85,10 +115,26 @@ function sshterm_display($admin) {
 $output = array();
 // only if admin
 if ($OPENQRM_USER->role == "administrator") {
-	$output[] = array('label' => 'SshTerm Manger', 'value' => sshterm_display("admin"));
+	$output[] = array('label' => 'SshTerm Manger', 'value' => sshterm_display());
+
+	if(htmlobject_request('action') != '') {
+		$strMsg = '';
+		switch (htmlobject_request('action')) {
+			case 'login':
+				foreach($_REQUEST['identifier'] as $id) {
+					if ("$id" != 0) {
+						$resource = new resource();
+						$resource->get_instance_by_id($id);
+						$ip = $resource->ip;
+						sshterm_login($id, $ip);
+					} else {
+						sshterm_login(0, localhost);
+					}
+				}
+				break;
+		}
+	}
 }
-
-
 
 
 echo htmlobject_tabmenu($output);
