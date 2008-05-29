02e542a009833e762e9575639cb47a5e592f43d2
mattr_sf <mattr_sf@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2008-05-29 17:35:29 +0000

    adapted the lvm-storage plugin to the new storage environment
    git-svn-id: http://openqrm.svn.sourceforge.net/svnroot/openqrm@596 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

--- trunk/src/plugins/lvm-storage/web/lvm-storage-manager.php
+++ trunk/src/plugins/lvm-storage/web/lvm-storage-manager.php
@@ -11,8 +11,8 @@ $BaseDir = $_SERVER["DOCUMENT_ROOT"].'/openqrm/';
 require_once "$RootDir/include/user.inc.php";
 require_once "$RootDir/class/image.class.php";
 require_once "$RootDir/class/storage.class.php";
+require_once "$RootDir/class/storagetype.class.php";
 require_once "$RootDir/class/resource.class.php";
-require_once "$RootDir/class/deployment.class.php";
 require_once "$RootDir/include/htmlobject.inc.php";
 
 
@@ -80,8 +80,8 @@ function lvm_select_storage() {
 	$arHead['storage_resource_ip'] = array();
 	$arHead['storage_resource_ip']['title'] ='Ip';
 
-	$arHead['storage_deployment_type'] = array();
-	$arHead['storage_deployment_type']['title'] ='Deployment';
+	$arHead['storage_type'] = array();
+	$arHead['storage_type']['title'] ='Type';
 
 	$arHead['storage_comment'] = array();
 	$arHead['storage_comment']['title'] ='Comment';
@@ -98,19 +98,10 @@ function lvm_select_storage() {
 		$storage->get_instance_by_id($storage_db["storage_id"]);
 		$storage_resource = new resource();
 		$storage_resource->get_instance_by_id($storage->resource_id);
-		$storage_deployment = new deployment();
-		$storage_deployment->get_instance_by_id($storage->deployment_type);
-		// is netapp ?
-		$cap_array = explode(" ", $storage->capabilities);
-		foreach ($cap_array as $index => $capabilities) {
-			if (strstr($capabilities, "STORAGE_TYPE")) {
-				$STORAGE_TYPE=str_replace("STORAGE_TYPE=\\\"", "", $capabilities);
-				$STORAGE_TYPE=str_replace("\\\"", "", $STORAGE_TYPE);
-				$STORAGE_TYPE=str_replace("STORAGE_TYPE=\"", "", $STORAGE_TYPE);
-				$STORAGE_TYPE=str_replace("\"", "", $STORAGE_TYPE);
-			}
-		}
-		if ("$STORAGE_TYPE" == "lvm-storage") {
+		$storage_type = new storagetype();
+		$storage_type->get_instance_by_id($storage->type);
+		// is lvm-storage ?
+		if ("$storage_type->name" == "lvm-storage") {
 			$storage_count++;
 			$resource_icon_default="/openqrm/base/img/resource.png";
 			$storage_icon="/openqrm/base/plugins/lvm-storage/img/storage.png";
@@ -128,7 +119,7 @@ function lvm_select_storage() {
 				'storage_name' => $storage->name,
 				'storage_resource_id' => $storage->resource_id,
 				'storage_resource_ip' => $storage_resource->ip,
-				'storage_deployment_type' => "$storage->deployment_type/$storage_deployment->type",
+				'storage_type' => "$storage->type/$storage_type->name",
 				'storage_comment' => $storage_resource->comment,
 				'storage_capabilities' => $storage_resource->capabilities,
 			);
@@ -162,10 +153,10 @@ function lvm_storage_display($lvm_storage_id) {
 	$storage->get_instance_by_id($lvm_storage_id);
 	$storage_resource = new resource();
 	$storage_resource->get_instance_by_id($storage->resource_id);
-	$storage_deployment = new deployment();
-	$storage_deployment->get_instance_by_id($storage->deployment_type);
+	$storage_type = new storagetype();
+	$storage_type->get_instance_by_id($storage->type);
 
-	$disp = "<h1>Lvm-storage Admin</h1>";
+	$disp = "<h1>Lvm-storage $storage->name</h1>";
 	$disp = $disp."<br>";
 	$disp = $disp."<br>";
 
@@ -189,8 +180,8 @@ function lvm_storage_display($lvm_storage_id) {
 	$arHead['storage_resource_ip'] = array();
 	$arHead['storage_resource_ip']['title'] ='Ip';
 
-	$arHead['storage_deployment_type'] = array();
-	$arHead['storage_deployment_type']['title'] ='Deployment';
+	$arHead['storage_type'] = array();
+	$arHead['storage_type']['title'] ='Type';
 
 	$arHead['storage_comment'] = array();
 	$arHead['storage_comment']['title'] ='Comment';
@@ -216,7 +207,7 @@ function lvm_storage_display($lvm_storage_id) {
 		'storage_name' => $storage->name,
 		'storage_resource_id' => $storage->resource_id,
 		'storage_resource_ip' => $storage_resource->ip,
-		'storage_deployment_type' => "$storage->deployment_type/$storage_deployment->type",
+		'storage_type' => "$storage->type/$storage_type->name",
 		'storage_comment' => $storage_resource->comment,
 		'storage_capabilities' => $storage_resource->capabilities,
 	);
@@ -275,10 +266,10 @@ function lvm_storage_lv_display($lvm_storage_id, $lvm_volume_group) {
 	$storage->get_instance_by_id($lvm_storage_id);
 	$storage_resource = new resource();
 	$storage_resource->get_instance_by_id($storage->resource_id);
-	$storage_deployment = new deployment();
-	$storage_deployment->get_instance_by_id($storage->deployment_type);
+	$storage_type = new storagetype();
+	$storage_type->get_instance_by_id($storage->type);
 
-	$disp = "<h1>Lvm-storage logical volume on $lvm_volume_group storage $lvm_storage_id</h1>";
+	$disp = "<h1>Lvm-storage logical volumes in $lvm_volume_group on $storage->name</h1>";
 	$disp = $disp."<br>";
 	$disp = $disp."<br>";
 
@@ -302,8 +293,8 @@ function lvm_storage_lv_display($lvm_storage_id, $lvm_volume_group) {
 	$arHead['storage_resource_ip'] = array();
 	$arHead['storage_resource_ip']['title'] ='Ip';
 
-	$arHead['storage_deployment_type'] = array();
-	$arHead['storage_deployment_type']['title'] ='Deployment';
+	$arHead['storage_type'] = array();
+	$arHead['storage_type']['title'] ='Type';
 
 	$arHead['storage_comment'] = array();
 	$arHead['storage_comment']['title'] ='Comment';
@@ -329,7 +320,7 @@ function lvm_storage_lv_display($lvm_storage_id, $lvm_volume_group) {
 		'storage_name' => $storage->name,
 		'storage_resource_id' => $storage->resource_id,
 		'storage_resource_ip' => $storage_resource->ip,
-		'storage_deployment_type' => "$storage->deployment_type/$storage_deployment->type",
+		'storage_type' => "$storage->type/$storage_type->name",
 		'storage_comment' => $storage_resource->comment,
 		'storage_capabilities' => $storage_resource->capabilities,
 	);
