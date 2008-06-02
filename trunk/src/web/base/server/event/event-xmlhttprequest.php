26c609fe2481b5d396aa874da668e0c468b15824
mattr_sf <mattr_sf@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2008-06-02 11:05:08 +0000

    do not report acknowledged error events
    git-svn-id: http://openqrm.svn.sourceforge.net/svnroot/openqrm@629 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

--- trunk/src/web/base/server/event/event-xmlhttprequest.php
+++ trunk/src/web/base/server/event/event-xmlhttprequest.php
@@ -10,7 +10,7 @@ require_once "$RootDir/class/event.class.php";
 $query = "select count(*) from $EVENT_INFO_TABLE";
 $event_total = openqrm_db_get_result_single ($query);
 
-$query = "select count(*) from $EVENT_INFO_TABLE where event_priority=1 OR event_priority=2 OR event_priority=3";
+$query = "select count(*) from $EVENT_INFO_TABLE where event_status<>1 AND event_priority=1 OR event_priority=2 OR event_priority=3";
 $event_error = openqrm_db_get_result_single ($query);
 
 
