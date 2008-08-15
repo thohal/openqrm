4ff0e904f9b7053e20625e0ae34857d256d26173
mattr_sf <mattr_sf@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2008-08-15 19:37:01 +0000

    added thisfile var
    git-svn-id: http://openqrm.svn.sourceforge.net/svnroot/openqrm@756 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

--- trunk/src/plugins/vmware-server/web/vmware-server-create.php
+++ trunk/src/plugins/vmware-server/web/vmware-server-create.php
@@ -4,7 +4,7 @@
 <?php
 
 // error_reporting(E_ALL);
-
+$thisfile = basename($_SERVER['PHP_SELF']);
 $RootDir = $_SERVER["DOCUMENT_ROOT"].'/openqrm/base/';
 $BaseDir = $_SERVER["DOCUMENT_ROOT"].'/openqrm/';
 require_once "$RootDir/include/user.inc.php";
