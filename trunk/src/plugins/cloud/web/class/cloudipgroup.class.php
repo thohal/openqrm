a259fe7611856eaebe0d8ee312cfa43ba1bd341f
mattr_sf <mattr_sf@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2008-11-05 22:30:15 +0000

    starting with network config
    git-svn-id: http://openqrm.svn.sourceforge.net/svnroot/openqrm@991 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

---
+++ trunk/src/plugins/cloud/web/class/cloudipgroup.class.php
@@ -0,0 +1,251 @@
+<?php
+
+// This class represents a cloud ipgroup in openQRM
+
+$RootDir = $_SERVER["DOCUMENT_ROOT"].'/openqrm/base/';
+require_once "$RootDir/include/openqrm-database-functions.php";
+require_once "$RootDir/class/resource.class.php";
+require_once "$RootDir/class/virtualization.class.php";
+require_once "$RootDir/class/image.class.php";
+require_once "$RootDir/class/kernel.class.php";
+require_once "$RootDir/class/plugin.class.php";
+require_once "$RootDir/class/event.class.php";
+
+$CLOUD_IPGROUP_TABLE="cloud_ipgroups";
+global $CLOUD_IPGROUP_TABLE;
+$event = new event();
+global $event;
+
+class cloudipgroup {
+
+var $ip_id = '';
+var $ip_ig_id = '';
+var $ip_appliance_id = '';
+var $ip_cr_id = '';
+var $ip_active = '';
+var $ip_address = '';
+var $ip_subnet = '';
+var $ip_gateway = '';
+var $ip_dns1 = '';
+var $ip_dns2 = '';
+
+
+// ---------------------------------------------------------------------------------
+// methods to create an instance of a cloudipgroup object filled from the db
+// ---------------------------------------------------------------------------------
+
+// returns an appliance from the db selected by id or name
+function get_instance($id, $name) {
+	global $CLOUD_IPGROUP_TABLE;
+	global $event;
+	$db=openqrm_get_db_connection();
+	if ("$id" != "") {
+		$cloudipgroup_array = &$db->Execute("select * from $CLOUD_IPGROUP_TABLE where ig_id=$id");
+	} else if ("$name" != "") {
+		$cloudipgroup_array = &$db->Execute("select * from $CLOUD_IPGROUP_TABLE where ig_name='$name'");
+	} else {
+		$event->log("get_instance", $_SERVER['REQUEST_TIME'], 2, "cloudipgroup.class.php", "Could not create instance of event without data", "", "", 0, 0, 0);
+		exit(-1);
+	}
+
+	foreach ($cloudipgroup_array as $index => $cloudipgroup) {
+		$this->ip_id = $cloudipgroup["ip_id"];
+		$this->ip_ig_id = $cloudipgroup["ip_ig_id"];
+		$this->ip_appliance_id = $cloudipgroup["ip_appliance_id"];
+		$this->ip_cr_id = $cloudipgroup["ip_cr_id"];
+		$this->ip_active = $cloudipgroup["ip_active"];
+		$this->ip_address = $cloudipgroup["ip_address"];
+		$this->ip_subnet = $cloudipgroup["ip_subnet"];
+		$this->ip_gateway = $cloudipgroup["ip_gateway"];
+		$this->ip_dns1 = $cloudipgroup["ip_dns1"];
+		$this->ip_dns2 = $cloudipgroup["ip_dns2"];
+	}
+	return $this;
+}
+
+// returns an appliance from the db selected by id
+function get_instance_by_id($id) {
+	$this->get_instance($id, "");
+	return $this;
+}
+
+// returns an appliance from the db selected by iname
+function get_instance_by_name($name) {
+	$this->get_instance("", $name);
+	return $this;
+}
+
+
+// ---------------------------------------------------------------------------------
+// general cloudipgroup methods
+// ---------------------------------------------------------------------------------
+
+
+
+
+// checks if given cloudipgroup id is free in the db
+function is_id_free($cloudipgroup_id) {
+	global $CLOUD_IPGROUP_TABLE;
+	global $event;
+	$db=openqrm_get_db_connection();
+	$rs = &$db->Execute("select cu_id from $CLOUD_IPGROUP_TABLE where ig_id=$cloudipgroup_id");
+	if (!$rs)
+		$event->log("is_id_free", $_SERVER['REQUEST_TIME'], 2, "cloudipgroup.class.php", $db->ErrorMsg(), "", "", 0, 0, 0);
+	else
+	if ($rs->EOF) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+
+// checks if given cloudipgroup name is free in the db
+function is_name_free($cloudipgroup_name) {
+	global $CLOUD_IPGROUP_TABLE;
+	global $event;
+	$db=openqrm_get_db_connection();
+	$rs = &$db->Execute("select cu_id from $CLOUD_IPGROUP_TABLE where ig_name='$cloudipgroup_name'");
+	if (!$rs)
+		$event->log("is_name_free", $_SERVER['REQUEST_TIME'], 2, "cloudipgroup.class.php", $db->ErrorMsg(), "", "", 0, 0, 0);
+	else
+	if ($rs->EOF) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+
+// adds cloudipgroup to the database
+function add($cloudipgroup_fields) {
+	global $CLOUD_IPGROUP_TABLE;
+	global $event;
+	if (!is_array($cloudipgroup_fields)) {
+		$event->log("add", $_SERVER['REQUEST_TIME'], 2, "cloudipgroup.class.php", "cloudipgroup_fields not well defined", "", "", 0, 0, 0);
+		return 1;
+	}
+	// set stop time and status to now
+	$now=$_SERVER['REQUEST_TIME'];
+	$db=openqrm_get_db_connection();
+	$result = $db->AutoExecute($CLOUD_IPGROUP_TABLE, $cloudipgroup_fields, 'INSERT');
+	if (! $result) {
+		$event->log("add", $_SERVER['REQUEST_TIME'], 2, "cloudipgroup.class.php", "Failed adding new cloudipgroup to database", "", "", 0, 0, 0);
+	}
+}
+
+
+
+// removes cloudipgroup from the database
+function remove($cloudipgroup_id) {
+	global $CLOUD_IPGROUP_TABLE;
+	$db=openqrm_get_db_connection();
+	$rs = $db->Execute("delete from $CLOUD_IPGROUP_TABLE where ig_id=$cloudipgroup_id");
+}
+
+// removes cloudipgroup from the database by cloudipgroup_name
+function remove_by_name($cloudipgroup_name) {
+	global $CLOUD_IPGROUP_TABLE;
+	$db=openqrm_get_db_connection();
+	$rs = $db->Execute("delete from $CLOUD_IPGROUP_TABLE where ig_name='$cloudipgroup_name'");
+}
+
+
+
+// returns cloudipgroup name by cloudipgroup_id
+function get_name($cloudipgroup_id) {
+	global $CLOUD_IPGROUP_TABLE;
+	global $event;
+	$db=openqrm_get_db_connection();
+	$cloudipgroup_set = &$db->Execute("select ig_name from $CLOUD_IPGROUP_TABLE where ig_id=$cloudipgroup_id");
+	if (!$cloudipgroup_set) {
+		$event->log("get_name", $_SERVER['REQUEST_TIME'], 2, "cloudipgroup.class.php", $db->ErrorMsg(), "", "", 0, 0, 0);
+	} else {
+		if (!$cloudipgroup_set->EOF) {
+			return $cloudipgroup_set->fields["ig_name"];
+		} else {
+			return "idle";
+		}
+	}
+}
+
+
+// returns the number of cloudipgroups for an cloudipgroup type
+function get_count() {
+	global $CLOUD_IPGROUP_TABLE;
+	$count=0;
+	$db=openqrm_get_db_connection();
+	$rs = $db->Execute("select count(ig_id) as num from $CLOUD_IPGROUP_TABLE");
+	if (!$rs) {
+		print $db->ErrorMsg();
+	} else {
+		$count = $rs->fields["num"];
+	}
+	return $count;
+}
+
+
+
+// returns a list of all cloudipgroup names
+function get_list() {
+	global $CLOUD_IPGROUP_TABLE;
+	$query = "select ig_id, ig_name from $CLOUD_IPGROUP_TABLE";
+	$cloudipgroup_name_array = array();
+	$cloudipgroup_name_array = openqrm_db_get_result_double ($query);
+	return $cloudipgroup_name_array;
+}
+
+
+// returns a list of all cloudipgroup ids
+function get_all_ids() {
+	global $CLOUD_IPGROUP_TABLE;
+	global $event;
+	$cloudipgroup_list = array();
+	$query = "select ig_id from $CLOUD_IPGROUP_TABLE";
+	$db=openqrm_get_db_connection();
+	$rs = $db->Execute($query);
+	if (!$rs)
+		$event->log("get_list", $_SERVER['REQUEST_TIME'], 2, "cloudipgroup.class.php", $db->ErrorMsg(), "", "", 0, 0, 0);
+	else
+	while (!$rs->EOF) {
+		$cloudipgroup_list[] = $rs->fields;
+		$rs->MoveNext();
+	}
+	return $cloudipgroup_list;
+
+}
+
+
+
+// displays the cloudipgroup-overview
+function display_overview($offset, $limit, $sort, $order) {
+	global $CLOUD_IPGROUP_TABLE;
+	global $event;
+	$db=openqrm_get_db_connection();
+	$recordSet = &$db->SelectLimit("select * from $CLOUD_IPGROUP_TABLE order by $sort $order", $limit, $offset);
+	$cloudipgroup_array = array();
+	if (!$recordSet) {
+		$event->log("display_overview", $_SERVER['REQUEST_TIME'], 2, "cloudipgroup.class.php", $db->ErrorMsg(), "", "", 0, 0, 0);
+	} else {
+		while (!$recordSet->EOF) {
+			array_push($cloudipgroup_array, $recordSet->fields);
+			$recordSet->MoveNext();
+		}
+		$recordSet->Close();
+	}		
+	return $cloudipgroup_array;
+}
+
+
+
+
+
+
+
+
+
+// ---------------------------------------------------------------------------------
+
+}
+
+?>
---
+++ trunk/src/plugins/cloud/web/cloud-ipgroup-manager.php
@@ -0,0 +1,198 @@
+
+<link rel="stylesheet" type="text/css" href="../../css/htmlobject.css" />
+
+<?php
+
+// error_reporting(E_ALL);
+$thisfile = basename($_SERVER['PHP_SELF']);
+$RootDir = $_SERVER["DOCUMENT_ROOT"].'/openqrm/base/';
+$BaseDir = $_SERVER["DOCUMENT_ROOT"].'/openqrm/';
+$CloudDir = $_SERVER["DOCUMENT_ROOT"].'/cloud-portal/';
+require_once "$RootDir/include/user.inc.php";
+require_once "$RootDir/class/image.class.php";
+require_once "$RootDir/class/resource.class.php";
+require_once "$RootDir/class/virtualization.class.php";
+require_once "$RootDir/class/appliance.class.php";
+require_once "$RootDir/class/deployment.class.php";
+require_once "$RootDir/class/openqrm_server.class.php";
+require_once "$RootDir/include/htmlobject.inc.php";
+// special clouduser class
+require_once "$RootDir/plugins/cloud/class/cloudconfig.class.php";
+require_once "$RootDir/plugins/cloud/class/cloudipgroup.class.php";
+
+global $OPENQRM_SERVER_BASE_DIR;
+global $CLOUD_IPGROUP_TABLE;
+$refresh_delay=5;
+
+// gather ipgroup parameter in array
+foreach ($_REQUEST as $key => $value) {
+	if (strncmp($key, "ig_", 3) == 0) {
+		$ipgroup_fields[$key] = $value;
+	}
+}
+
+// check if we got some actions to do
+if(htmlobject_request('action') != '') {
+	switch (htmlobject_request('action')) {
+		case 'delete':
+			foreach($_REQUEST['identifier'] as $id) {
+				$ig = new cloudipgroup();
+				$ig->remove($id);
+
+			}
+			break;
+
+		case 'create_ipgroup':
+			$ig_name = $ipgroup_fields['ig_name'];
+			echo "Creating IpGroup $ipgroup_name <br>";
+			
+			print_r($ipgroup_fields);
+			flush();
+			
+			$ig = new cloudipgroup();
+			$ipgroup_fields['ig_id'] = openqrm_db_get_free_id('ig_id', $CLOUD_IPGROUP_TABLE);
+			$ig->add($ipgroup_fields);
+
+
+			break;
+
+	}
+}
+
+
+
+
+function cloud_ipgroup_manager() {
+
+	global $OPENQRM_USER;
+	global $OPENQRM_SERVER_IP_ADDRESS;
+	global $thisfile;
+	$table = new htmlobject_db_table('ig_id');
+
+	$cc_conf = new cloudconfig();
+	// get external name
+	$external_portal_name = $cc_conf->get_value(3);  // 3 is the external name
+	if (!strlen($external_portal_name)) {
+		$external_portal_name = "http://$OPENQRM_SERVER_IP_ADDRESS/cloud-portal";
+	}
+
+	$disp = "<h1>Cloud IpGroups for portal at <a href=\"$external_portal_name\">$external_portal_name</a></h1>";
+	$disp = $disp."<br>";
+	$disp = $disp."<br>";
+	$disp = $disp."<b><a href=\"$thisfile?action=create\">Create new Cloud IpGroup</a></b>";
+	$disp = $disp."<br>";
+	$arHead = array();
+
+	$arHead['ig_id'] = array();
+	$arHead['ig_id']['title'] ='ID';
+
+	$arHead['ig_name'] = array();
+	$arHead['ig_name']['title'] ='Name';
+
+	$arHead['ig_start'] = array();
+	$arHead['ig_start']['title'] ='1. Ip';
+
+	$arHead['ig_stop'] = array();
+	$arHead['ig_stop']['title'] ='Last Ip';
+
+	$arHead['ig_subnet'] = array();
+	$arHead['ig_subnet']['title'] ='Subnet';
+
+	$arHead['ig_gateway'] = array();
+	$arHead['ig_gateway']['title'] ='Gateway';
+
+	$arHead['ig_dns1'] = array();
+	$arHead['ig_dns1']['title'] ='1. DNS';
+
+	$arHead['ig_dns2'] = array();
+	$arHead['ig_dns2']['title'] ='2. DNS';
+
+	$arBody = array();
+
+	// db select
+	$ig = new cloudipgroup();
+	$ig_array = array();
+	$ig_array = $ig->display_overview(0, 100, 'ig_id', 'ASC');
+	foreach ($ig_array as $index => $ipg) {
+		$arBody[] = array(
+			'ig_id' => $ipg["ig_id"],
+			'ig_name' => $ipg["ig_name"],
+			'ig_start' => $ipg["ig_start"],
+			'ig_stop' => $ipg["ig_stop"],
+			'ig_subnet' => $ipg["ig_subnet"],
+			'ig_gateway' => $ipg["ig_gateway"],
+			'ig_dns1' => $ipg["ig_dns1"],
+			'ig_dns2' => $ipg["ig_dns2"],
+		);
+	}
+
+	$table->id = 'Tabelle';
+	$table->css = 'htmlobject_table';
+	$table->border = 1;
+	$table->cellspacing = 0;
+	$table->cellpadding = 3;
+	$table->form_action = $thisfile;
+	$table->identifier_type = "checkbox";
+	$table->head = $arHead;
+	$table->body = $arBody;
+	if ($OPENQRM_USER->role == "administrator") {
+		$table->bottom = array('delete');
+		$table->identifier = 'ig_id';
+	}
+	$table->max = 100;
+	return $disp.$table->get_string();
+}
+
+
+function cloud_create_ipgroup() {
+
+	global $OPENQRM_USER;
+	global $thisfile;
+
+
+	$disp = "<h1>Create new Cloud IpGroup</h1>";
+	$disp = $disp."<br>";
+	$disp = $disp."<br>";
+	$disp = $disp."<br>";
+	$disp = $disp."<br>";
+	$disp = $disp."<form action=$thisfile method=post>";
+	$disp = $disp.htmlobject_input('ig_name', array("value" => '[IpGroup-Name]', "label" => 'Name'), 'text', 20);
+	$disp = $disp.htmlobject_input('ig_start', array("value" => '[start-ip-address]', "label" => 'Start-Ip'), 'text', 20);
+	$disp = $disp.htmlobject_input('ig_stop', array("value" => '[stop-ip-address]', "label" => 'Stop-Ip'), 'text', 20);
+	$disp = $disp.htmlobject_input('ig_subnet', array("value" => '[subnet-mask]', "label" => 'Subnet'), 'text', 20);
+	$disp = $disp.htmlobject_input('ig_gateway', array("value" => '[gateway]', "label" => 'Gateway'), 'text', 20);
+	$disp = $disp.htmlobject_input('ig_dns1', array("value" => '[fist-dns-server]', "label" => '1. DNS'), 'text', 20);
+	$disp = $disp.htmlobject_input('ig_dns2', array("value" => '[second-dns-server]', "label" => '2. DNS'), 'text', 20);
+
+	$disp = $disp."<input type=hidden name='action' value='create_ipgroup'>";
+	$disp = $disp."<br>";
+	$disp = $disp."<input type=submit value='Create'>";
+	$disp = $disp."<br>";
+	$disp = $disp."<br>";
+	$disp = $disp."</form>";
+
+
+
+	return $disp;
+}
+
+
+
+
+$output = array();
+
+
+if(htmlobject_request('action') != '') {
+	switch (htmlobject_request('action')) {
+		case 'create':
+			$output[] = array('label' => 'Create Cloud IpGroup', 'value' => cloud_create_ipgroup());
+			break;
+		default:
+			$output[] = array('label' => 'Cloud Manager', 'value' => cloud_ipgroup_manager());
+			break;
+	}
+} else {
+	$output[] = array('label' => 'Cloud Manager', 'value' => cloud_ipgroup_manager());
+}
+echo htmlobject_tabmenu($output);
+?>
