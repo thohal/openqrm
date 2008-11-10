215a7c60021ebffbb9da7fabe925a24d47609438
akuballa <akuballa@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2008-11-10 14:47:50 +0000

    added doc block
    git-svn-id: http://openqrm.svn.sourceforge.net/svnroot/openqrm@997 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

--- trunk/src/web/base/class/storage.class.php
+++ trunk/src/web/base/class/storage.class.php
@@ -1,231 +1,331 @@
 <?php
+/**
+ * @copyright Copyright (c) 2008, openqrm
+ * @license see openqrm licence
+ * @package base
+ * @author Matt Rechenburg <mattr_sf@users.sourceforge.net>
+ * @version 1.0
+ * @author Alexander Kuballa <akuballa@users.sourceforge.net>
+ * @version 1.1 added documentation
+ */
 
-// This class represents a storage server
-
-$RootDir = $_SERVER["DOCUMENT_ROOT"].'/openqrm/base/';
-require_once "$RootDir/include/openqrm-database-functions.php";
-require_once "$RootDir/class/event.class.php";
-
-global $STORAGE_INFO_TABLE;
-$event = new event();
-global $event;
 
 class storage {
-
+/**
+* storage id
+* @access public
+* @var int
+*/
 var $id = '';
+/**
+* storage name
+* @access public
+* @var string
+*/
 var $name = '';
+/**
+* resource id used by storage
+* @access public
+* @var int
+*/
 var $resource_id = '';
+/**
+* storage type
+* @access public
+* @var string
+*/
 var $type = '';
+/**
+* state of storage
+* @access public
+* @var string
+*/
 var $state = '';
+/**
+* comment for storage
+* @access public
+* @var string
+*/
 var $comment = '';
+/**
+* storage capabilities
+* @access public
+* @var string
+*/
 var $capabilities = '';
 
+//----------------------------------------------- private section
+/**
+* name of database table
+* @access public
+* @var string
+*/
+var $_db_table;
 
+/**
+* event object
+* @access public
+* @var object
+*/
+var $_event;
 
-// ---------------------------------------------------------------------------------
-// methods to create an instance of a storage object filled from the db
-// ---------------------------------------------------------------------------------
-
-// returns a storage from the db selected by id or name
-function get_instance($id, $name) {
-	global $STORAGE_INFO_TABLE;
-	global $event;
-	$db=openqrm_get_db_connection();
-	if ("$id" != "") {
-		$storage_array = &$db->Execute("select * from $STORAGE_INFO_TABLE where storage_id=$id");
-	} else if ("$name" != "") {
-		$storage_array = &$db->Execute("select * from $STORAGE_INFO_TABLE where storage_name='$name'");
-	} else {
-		$event->log("get_instance", $_SERVER['REQUEST_TIME'], 2, "storage.class.php", "Could not create instance of storage without data", "", "", 0, 0, 0);
-		exit(-1);
+	function storage() {
+		$this->init();
 	}
-	foreach ($storage_array as $index => $storage) {
-		$this->id = $storage["storage_id"];
-		$this->name = $storage["storage_name"];
-		$this->resource_id = $storage["storage_resource_id"];
-		$this->type = $storage["storage_type"];
-		$this->state = $storage["storage_state"];
-		$this->comment = $storage["storage_comment"];
-		$this->capabilities = $storage["storage_capabilities"];
+
+	//--------------------------------------------------
+	/**
+	* init storage environment
+	* @access public
+	*/
+	//--------------------------------------------------
+	function init() {
+		global $STORAGE_INFO_TABLE;
+		$RootDir = $_SERVER["DOCUMENT_ROOT"].'/openqrm/base/';
+		require_once "$RootDir/include/openqrm-database-functions.php";
+		require_once "$RootDir/class/event.class.php";
+		$this->_db_table = $STORAGE_INFO_TABLE;
+		$this->_event = new event();
 	}
-	return $this;
-}
 
-// returns a storage from the db selected by id
-function get_instance_by_id($id) {
-	$this->get_instance($id, "");
-	return $this;
-}
-
-// returns a storage from the db selected by iname
-function get_instance_by_name($name) {
-	$this->get_instance("", $name);
-	return $this;
-}
-
-
-// ---------------------------------------------------------------------------------
-// general storage methods
-// ---------------------------------------------------------------------------------
-
-
-
-
-// checks if given storage id is free in the db
-function is_id_free($storage_id) {
-	global $STORAGE_INFO_TABLE;
-	global $event;
-	$db=openqrm_get_db_connection();
-	$rs = &$db->Execute("select storage_id from $STORAGE_INFO_TABLE where storage_id=$storage_id");
-	if (!$rs)
-		$event->log("is_id_free", $_SERVER['REQUEST_TIME'], 2, "storage.class.php", $db->ErrorMsg(), "", "", 0, 0, 0);
-	else
-	if ($rs->EOF) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-
-// adds storage to the database
-function add($storage_fields) {
-	global $STORAGE_INFO_TABLE;
-	global $event;
-	if (!is_array($storage_fields)) {
-		$event->log("add", $_SERVER['REQUEST_TIME'], 2, "storage.class.php", "Storage_field not well defined", "", "", 0, 0, 0);
-		return 1;
-	}
-	$db=openqrm_get_db_connection();
-	$result = $db->AutoExecute($STORAGE_INFO_TABLE, $storage_fields, 'INSERT');
-	if (! $result) {
-		$event->log("add", $_SERVER['REQUEST_TIME'], 2, "storage.class.php", "Failed adding new storage to database", "", "", 0, 0, 0);
-	}
-}
-
-
-
-// updates storage in the database
-function update($storage_id, $storage_fields) {
-	global $STORAGE_INFO_TABLE;
-	global $event;
-	if ($storage_id < 0 || ! is_array($storage_fields)) {
-		$event->log("update", $_SERVER['REQUEST_TIME'], 2, "storage.class.php", "Unable to update storage $storage_id", "", "", 0, 0, 0);
-		return 1;
-	}
-	$db=openqrm_get_db_connection();
-	unset($storage_fields["storage_id"]);
-	$result = $db->AutoExecute($STORAGE_INFO_TABLE, $storage_fields, 'UPDATE', "storage_id = $storage_id");
-	if (! $result) {
-		$event->log("update", $_SERVER['REQUEST_TIME'], 2, "storage.class.php", "Failed updating storage $storage_id", "", "", 0, 0, 0);
-	}
-}
-
-// removes storage from the database
-function remove($storage_id) {
-	global $STORAGE_INFO_TABLE;
-	$db=openqrm_get_db_connection();
-	$rs = $db->Execute("delete from $STORAGE_INFO_TABLE where storage_id=$storage_id");
-}
-
-// removes storage from the database by storage_name
-function remove_by_name($storage_name) {
-	global $STORAGE_INFO_TABLE;
-	$db=openqrm_get_db_connection();
-	$rs = $db->Execute("delete from $STORAGE_INFO_TABLE where storage_name='$storage_name'");
-}
-
-// returns storage name by storage_id
-function get_name($storage_id) {
-	global $STORAGE_INFO_TABLE;
-	global $event;
-	$db=openqrm_get_db_connection();
-	$storage_set = &$db->Execute("select storage_name from $STORAGE_INFO_TABLE where storage_id=$storage_id");
-	if (!$storage_set) {
-		$event->log("get_name", $_SERVER['REQUEST_TIME'], 2, "storage.class.php", $db->ErrorMsg(), "", "", 0, 0, 0);
-	} else {
-		if (!$storage_set->EOF) {
-			return $storage_set->fields["storage_name"];
+	//--------------------------------------------------
+	/**
+	* get an instance of a storage object from db
+	* @access public
+	* @param int $id
+	* @param string $name
+	* @return object
+	*/
+	//--------------------------------------------------
+	function get_instance($id, $name) {
+		$db=openqrm_get_db_connection();
+		if ("$id" != "") {
+			$storage_array = &$db->Execute("select * from $this->_db_table where storage_id=$id");
+		} else if ("$name" != "") {
+			$storage_array = &$db->Execute("select * from $this->_db_table where storage_name='$name'");
 		} else {
-			return "idle";
+			$this->_event->log("get_instance", $_SERVER['REQUEST_TIME'], 2, "storage.class.php", "Could not create instance of storage without data", "", "", 0, 0, 0);
+			exit(-1);
+		}
+		foreach ($storage_array as $index => $storage) {
+			$this->id = $storage["storage_id"];
+			$this->name = $storage["storage_name"];
+			$this->resource_id = $storage["storage_resource_id"];
+			$this->type = $storage["storage_type"];
+			$this->state = $storage["storage_state"];
+			$this->comment = $storage["storage_comment"];
+			$this->capabilities = $storage["storage_capabilities"];
+		}
+		return $this;
+	}
+
+	//--------------------------------------------------
+	/**
+	* get an instance of a storage by id
+	* @access public
+	* @param int $id
+	* @return object
+	*/
+	//--------------------------------------------------
+	function get_instance_by_id($id) {
+		$this->get_instance($id, "");
+		return $this;
+	}
+
+	//--------------------------------------------------
+	/**
+	* get an instance of a storage by name
+	* @access public
+	* @param string $name
+	* @return object
+	*/
+	//--------------------------------------------------
+	function get_instance_by_name($name) {
+		$this->get_instance("", $name);
+		return $this;
+	}
+
+	//--------------------------------------------------
+	/**
+	* add a new storage
+	* @access public
+	* @param array $storage_fields
+	*/
+	//--------------------------------------------------
+	function add($storage_fields) {
+		if (!is_array($storage_fields)) {
+			$this->_event->log("add", $_SERVER['REQUEST_TIME'], 2, "storage.class.php", "Storage_field not well defined", "", "", 0, 0, 0);
+			return 1;
+		}
+		$db=openqrm_get_db_connection();
+		$result = $db->AutoExecute($this->_db_table, $storage_fields, 'INSERT');
+		if (! $result) {
+			$this->_event->log("add", $_SERVER['REQUEST_TIME'], 2, "storage.class.php", "Failed adding new storage to database", "", "", 0, 0, 0);
 		}
 	}
-}
 
-// returns capabilities string by storage_id
-function get_capabilities($storage_id) {
-	global $STORAGE_INFO_TABLE;
-	global $event;
-	$db=openqrm_get_db_connection();
-	$storage_set = &$db->Execute("select storage_capabilities from $STORAGE_INFO_TABLE where storage_id=$storage_id");
-	if (!$storage_set) {
-		$event->log("get_capabilities", $_SERVER['REQUEST_TIME'], 2, "storage.class.php", $db->ErrorMsg(), "", "", 0, 0, 0);
-	} else {
-		if ((!$storage_set->EOF) && ($storage_set->fields["storage_capabilities"]!=""))  {
-			return $storage_set->fields["storage_capabilities"];
+	//--------------------------------------------------
+	/**
+	* update a storage
+	* <code>
+	* $fields = array();
+	* $fields['storage_name'] = 'somename';
+	* $fields['storage_type'] = 1;
+	* $fields['storage_capabilities'] = 'sometext';
+	* $fields['storage_comment'] = 'sometext';
+	* $fields['storage_resource_id'] = 1;
+	* $storage = new storage();
+	* $storage->update(1, $fields);
+	* </code>
+	* @access public
+	* @param int $storage_id
+	* @param array $storage_fields
+	* @return bool
+	*/
+	//--------------------------------------------------
+	function update($storage_id, $storage_fields) {
+		if ($storage_id < 0 || ! is_array($storage_fields)) {
+			$this->_event->log("update", $_SERVER['REQUEST_TIME'], 2, "storage.class.php", "Unable to update storage $storage_id", "", "", 0, 0, 0);
+		}
+		$db=openqrm_get_db_connection();
+		unset($storage_fields["storage_id"]);
+		$result = $db->AutoExecute($this->_db_table, $storage_fields, 'UPDATE', "storage_id = $storage_id");
+		if (! $result) {
+			$this->_event->log("update", $_SERVER['REQUEST_TIME'], 2, "storage.class.php", "Failed updating storage $storage_id", "", "", 0, 0, 0);
+			return false;
 		} else {
-			return "0";
+			return true;
 		}
 	}
-}
 
-// returns the number of storages for an storage type
-function get_count() {
-	global $STORAGE_INFO_TABLE;
-	global $event;
-	$count=0;
-	$db=openqrm_get_db_connection();
-	$rs = $db->Execute("select count(storage_id) as num from $STORAGE_INFO_TABLE");
-	if (!$rs) {
-		$event->log("get_count", $_SERVER['REQUEST_TIME'], 2, "storage.class.php", $db->ErrorMsg(), "", "", 0, 0, 0);
-	} else {
-		$count = $rs->fields["num"];
+	//--------------------------------------------------
+	/**
+	* remove a storage by id
+	* @access public
+	* @param int $storage_id
+	*/
+	//--------------------------------------------------
+	function remove($storage_id) {
+		$db=openqrm_get_db_connection();
+		$rs = $db->Execute("delete from $this->_db_table where storage_id=$storage_id");
 	}
-	return $count;
-}
 
+	//--------------------------------------------------
+	/**
+	* remove a storage by name
+	* @access public
+	* @param int $storage_name
+	*/
+	//--------------------------------------------------
+	function remove_by_name($storage_name) {
+		$db=openqrm_get_db_connection();
+		$rs = $db->Execute("delete from $this->_db_table where storage_name='$storage_name'");
+	}
 
-
-// returns a list of all storage names
-function get_list() {
-	global $STORAGE_INFO_TABLE;
-	$query = "select storage_id, storage_name from $STORAGE_INFO_TABLE";
-	$storage_name_array = array();
-	$storage_name_array = openqrm_db_get_result_double ($query);
-	return $storage_name_array;
-}
-
-
-
-// displays the storage-overview
-function display_overview($offset, $limit, $sort, $order) {
-	global $STORAGE_INFO_TABLE;
-	global $event;
-	$db=openqrm_get_db_connection();
-	$recordSet = &$db->SelectLimit("select * from $STORAGE_INFO_TABLE order by $sort $order", $limit, $offset);
-	$storage_array = array();
-	if (!$recordSet) {
-		$event->log("display_overview", $_SERVER['REQUEST_TIME'], 2, "storage.class.php", $db->ErrorMsg(), "", "", 0, 0, 0);
-	} else {
-		while (!$recordSet->EOF) {
-			array_push($storage_array, $recordSet->fields);
-			$recordSet->MoveNext();
+	//--------------------------------------------------
+	/**
+	* get a storage name by id
+	* @access public
+	* @param int $storage_id
+	*/
+	//--------------------------------------------------
+	function get_name($storage_id) {
+		$db=openqrm_get_db_connection();
+		$storage_set = &$db->Execute("select storage_name from $this->_db_table where storage_id=$storage_id");
+		if (!$storage_set) {
+			$this->_event->log("get_name", $_SERVER['REQUEST_TIME'], 2, "storage.class.php", $db->ErrorMsg(), "", "", 0, 0, 0);
+		} else {
+			if (!$storage_set->EOF) {
+				return $storage_set->fields["storage_name"];
+			} else {
+				return "idle";
+			}
 		}
-		$recordSet->Close();
-	}		
-	return $storage_array;
+	}
+
+	//--------------------------------------------------
+	/**
+	* get capabilities string by storage_id
+	* @access public
+	* @param int $storage_id
+	* @return string
+	*/
+	//--------------------------------------------------
+	function get_capabilities($storage_id) {
+		$db=openqrm_get_db_connection();
+		$storage_set = &$db->Execute("select storage_capabilities from $this->_db_table where storage_id=$storage_id");
+		if (!$storage_set) {
+			$this->_event->log("get_capabilities", $_SERVER['REQUEST_TIME'], 2, "storage.class.php", $db->ErrorMsg(), "", "", 0, 0, 0);
+		} else {
+			if ((!$storage_set->EOF) && ($storage_set->fields["storage_capabilities"]!=""))  {
+				return $storage_set->fields["storage_capabilities"];
+			} else {
+				return "0";
+			}
+		}
+	}
+
+	//--------------------------------------------------
+	/**
+	* get number of storages
+	* @access public
+	* @return int
+	*/
+	//--------------------------------------------------
+	function get_count() {
+		$count=0;
+		$db=openqrm_get_db_connection();
+		$rs = $db->Execute("select count(storage_id) as num from $this->_db_table");
+		if (!$rs) {
+			$this->_event->log("get_count", $_SERVER['REQUEST_TIME'], 2, "storage.class.php", $db->ErrorMsg(), "", "", 0, 0, 0);
+		} else {
+			$count = $rs->fields["num"];
+		}
+		return $count;
+	}
+
+	//--------------------------------------------------
+	/**
+	* get an array of all storage names
+	* <code>
+	* $storage = new storage();
+	* $arr = $storage->get_list();
+	* // $arr[0]['id']
+	* // $arr[0]['name']
+	* </code>
+	* @access public
+	* @return array
+	*/
+	//--------------------------------------------------
+	function get_list() {
+		$query = "select storage_id, storage_name from $this->_db_table";
+		$storage_name_array = array();
+		$storage_name_array = openqrm_db_get_result_double ($query);
+		return $storage_name_array;
+	}
+
+	//--------------------------------------------------
+	/**
+	* get an array of storages
+	* @access public
+	* @return array
+	*/
+	//--------------------------------------------------
+	function display_overview($offset, $limit, $sort, $order) {
+		$db=openqrm_get_db_connection();
+		$recordSet = &$db->SelectLimit("select * from $this->_db_table order by $sort $order", $limit, $offset);
+		$storage_array = array();
+		if (!$recordSet) {
+			$this->_event->log("display_overview", $_SERVER['REQUEST_TIME'], 2, "storage.class.php", $db->ErrorMsg(), "", "", 0, 0, 0);
+		} else {
+			while (!$recordSet->EOF) {
+				array_push($storage_array, $recordSet->fields);
+				$recordSet->MoveNext();
+			}
+			$recordSet->Close();
+		}		
+		return $storage_array;
+	}
+
 }
-
-
-
-
-
-
-
-
-
-// ---------------------------------------------------------------------------------
-
-}
-
 ?>
