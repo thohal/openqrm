c4494aeaf49a20c194f125f9b7eb1e57ba6192b6
mattr_sf <mattr_sf@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2008-11-14 17:07:12 +0000

    set default limit to 50 in the plugin manager
    git-svn-id: http://openqrm.svn.sourceforge.net/svnroot/openqrm@1035 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

--- trunk/src/web/base/plugins/aa_plugins/plugin-manager.php
+++ trunk/src/web/base/plugins/aa_plugins/plugin-manager.php
@@ -233,6 +233,7 @@ $table_1->add_headrow(htmlobject_select('plugin_filter', $plugs, 'Filter by Type
 $table_1->id = 'Tabelle';
 $table_1->css = 'htmlobject_table';
 $table_1->border = 1;
+$table_1->limit = 50;
 $table_1->cellspacing = 0;
 $table_1->cellpadding = 3;
 $table_1->form_action = $thisfile;
