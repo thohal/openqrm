c53d8b89ef51258ea2315a92d5fe4f1647150ddb
mattr_sf <mattr_sf@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2008-11-23 16:44:54 +0000

    working better in the background
    git-svn-id: http://openqrm.svn.sourceforge.net/svnroot/openqrm@1065 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

--- trunk/src/plugins/nfs-storage/web/openqrm-nfs-deployment-auth-hook.php
+++ trunk/src/plugins/nfs-storage/web/openqrm-nfs-deployment-auth-hook.php
@@ -9,17 +9,74 @@ require_once "$RootDir/class/resource.class.php";
 require_once "$RootDir/class/image.class.php";
 require_once "$RootDir/class/storage.class.php";
 require_once "$RootDir/class/deployment.class.php";
+require_once "$RootDir/class/appliance.class.php";
 require_once "$RootDir/class/openqrm_server.class.php";
 require_once "$RootDir/include/openqrm-server-config.php";
 global $OPENQRM_SERVER_BASE_DIR;
 global $OPENQRM_EXEC_PORT;
+$openqrm_server = new openqrm_server();
+$OPENQRM_SERVER_IP_ADDRESS=$openqrm_server->get_ip_address();
 global $OPENQRM_SERVER_IP_ADDRESS;
+global $openqrm_server;
 $event = new event();
 global $event;
 
 
 
 function storage_auth_function($cmd, $appliance_id) {
+	global $event;
+	global $OPENQRM_SERVER_BASE_DIR;
+	global $OPENQRM_SERVER_IP_ADDRESS;
+	global $OPENQRM_EXEC_PORT;
+	global $openqrm_server;
+
+	$appliance = new appliance();
+	$appliance->get_instance_by_id($appliance_id);
+
+	$image = new image();
+	$image->get_instance_by_id($appliance->imageid);
+	$image_name=$image->name;
+	$image_rootdevice=$image->rootdevice;
+
+	$storage = new storage();
+	$storage->get_instance_by_id($image->storageid);
+	$storage_resource = new resource();
+	$storage_resource->get_instance_by_id($storage->resource_id);
+	$storage_ip = $storage_resource->ip;
+
+	$deployment = new deployment();
+	$deployment->get_instance_by_type($image->type);
+	$deployment_type = $deployment->type;
+	$deployment_plugin_name = $deployment->storagetype;
+
+	$resource = new resource();
+	$resource->get_instance_by_id($appliance->resources);
+	$resource_ip=$resource->ip;
+
+	switch($cmd) {
+		case "start":
+			$event->log("openqrm_new_appliance", $_SERVER['REQUEST_TIME'], 5, "openqrm-dns-appliance-hook.php", "Authenticating $image_name to resource $resource_ip", "", "", 0, 0, $appliance_id);
+			$auth_start_cmd = "$OPENQRM_SERVER_BASE_DIR/openqrm/plugins/$deployment_plugin_name/bin/openqrm-$deployment_plugin_name auth -n $image_rootdevice -i $resource_ip";
+			$resource->send_command($storage_ip, $auth_start_cmd);
+			break;
+		case "stop":
+			$stop_hook_file = "/tmp/openqrm-nfs-deployment-auth-hook.$appliance_id";
+			$auth_stop_cmd = "(ln -s $OPENQRM_SERVER_BASE_DIR/openqrm/plugins/nfs-storage/web/openqrm-nfs-deployment-auth-hook.php $OPENQRM_SERVER_BASE_DIR/openqrm/web/boot-service/openqrm-nfs-deployment-auth-hook.$appliance_id.php && wget -q -O /dev/null \"http://localhost/openqrm/boot-service/openqrm-nfs-deployment-auth-hook.$appliance_id.php?bgcmd=stop_auth&appliance_id=$appliance_id\" && rm -f $OPENQRM_SERVER_BASE_DIR/openqrm/web/boot-service/openqrm-nfs-deployment-auth-hook.$appliance_id.php $stop_hook_file) &";
+			$fp = fopen($stop_hook_file, 'w');
+			fwrite($fp, $auth_stop_cmd);
+			fclose($fp);
+			chmod($stop_hook_file, 0750);
+			$openqrm_server->send_command($stop_hook_file);
+			break;
+		
+	}
+
+}
+
+
+
+function storage_auth_stop_in_background($appliance_id) {
+
 	global $event;
 	global $OPENQRM_SERVER_BASE_DIR;
 	global $OPENQRM_SERVER_IP_ADDRESS;
@@ -48,38 +105,35 @@ function storage_auth_function($cmd, $appliance_id) {
 	$resource->get_instance_by_id($appliance->resources);
 	$resource_ip=$resource->ip;
 
-	$event->log("openqrm_new_appliance", $_SERVER['REQUEST_TIME'], 5, "openqrm-dns-appliance-hook.php", "Authenticating $image_name to resource $resource_ip", "", "", 0, 0, $appliance_id);
-
-	switch($cmd) {
-		case "start":
-			$auth_start_cmd = "$OPENQRM_SERVER_BASE_DIR/openqrm/plugins/$deployment_plugin_name/bin/openqrm-$deployment_plugin_name auth -n $image_rootdevice -i $resource_ip";
-			$resource->send_command($storage_ip, $auth_start_cmd);
-			break;
-		case "stop":
-			$loop=0;
-			while(1) {
-				$resource->get_instance_by_id($appliance->resources);
-				if (!strcmp($resource->state, "active")) {
-					$event->log("openqrm_new_appliance", $_SERVER['REQUEST_TIME'], 2, "openqrm-dns-appliance-hook.php", "Resource is idle again, applying stop auth", "", "", 0, 0, $appliance_id);
-					break;				
-				} else {
-					$event->log("openqrm_new_appliance", $_SERVER['REQUEST_TIME'], 2, "openqrm-dns-appliance-hook.php", "Resource in transition loop $loop", "", "", 0, 0, $appliance_id);
-				}
-				if ($loop > 10) {
-					$event->log("openqrm_new_appliance", $_SERVER['REQUEST_TIME'], 2, "openqrm-dns-appliance-hook.php", "Timeout for stop auth hook, exiting !", "", "", 0, 0, $appliance_id);
-					return;
-				}
-				sleep(10);
-				$loop++;
-			}
-			$auth_stop_cmd = "$OPENQRM_SERVER_BASE_DIR/openqrm/plugins/$deployment_plugin_name/bin/openqrm-$deployment_plugin_name auth -n $image_rootdevice -i $OPENQRM_SERVER_IP_ADDRESS";
-			$resource->send_command($storage_ip, $auth_stop_cmd);
-			break;
-		
+	$loop=0;
+	while(1) {
+		$resource->get_instance_by_id($appliance->resources);
+		if (!strcmp($resource->state, "active")) {
+			$event->log("openqrm_new_appliance", $_SERVER['REQUEST_TIME'], 5, "openqrm-dns-appliance-hook.php", "Resource $resource_ip is idle again, applying stop auth for image $image_name", "", "", 0, 0, $appliance_id);
+			break;				
+		}
+		if ($loop > 500) {
+			$event->log("openqrm_new_appliance", $_SERVER['REQUEST_TIME'], 2, "openqrm-dns-appliance-hook.php", "Timeout for stop auth hook, exiting !", "", "", 0, 0, $appliance_id);
+			return;
+		}
+		sleep(2);
+		$loop++;
 	}
+	$auth_stop_cmd = "$OPENQRM_SERVER_BASE_DIR/openqrm/plugins/$deployment_plugin_name/bin/openqrm-$deployment_plugin_name auth -n $image_rootdevice -i $OPENQRM_SERVER_IP_ADDRESS";
+	$resource->send_command($storage_ip, $auth_stop_cmd);
 
 }
 
+
+// do we run the background hook ?
+$bgcmd = $_REQUEST["bgcmd"];
+$appliance_id = $_REQUEST["appliance_id"];
+
+	switch ($bgcmd) {
+		case 'stop_auth':
+			storage_auth_stop_in_background($appliance_id);
+			break;
+	}
 
 
 ?>
