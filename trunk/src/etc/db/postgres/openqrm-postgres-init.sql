785803ace2e4bb0fe870eda066eba4ccbdbdc317
mattr_sf <mattr_sf@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2009-02-25 10:35:51 +0000

    fixed wrong table name in grant char -> varchar
    git-svn-id: http://openqrm.svn.sourceforge.net/svnroot/openqrm@1261 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

--- trunk/src/etc/db/postgres/openqrm-postgres-init.sql
+++ trunk/src/etc/db/postgres/openqrm-postgres-init.sql
@@ -3,98 +3,98 @@ drop table resource_info;
 create table resource_info(
 	resource_id int8 NOT NULL PRIMARY KEY,
 	resource_localboot int8,
-	resource_kernel char(50),
+	resource_kernel varchar(50),
 	resource_kernelid int8,
-	resource_image char(50),
+	resource_image varchar(50),
 	resource_imageid int8,
-	resource_openqrmserver char(20),
-	resource_basedir char(100),
+	resource_openqrmserver varchar(20),
+	resource_basedir varchar(100),
 	resource_applianceid int8,
-	resource_ip char(20),
-	resource_subnet char(20),
-	resource_broadcast char(20),
-	resource_network char(20),
-	resource_mac char(20),
+	resource_ip varchar(20),
+	resource_subnet varchar(20),
+	resource_broadcast varchar(20),
+	resource_network varchar(20),
+	resource_mac varchar(20),
 	resource_uptime int8,
 	resource_cpunumber int8,
 	resource_cpuspeed int8,
-	resource_cpumodel char(40),
+	resource_cpumodel varchar(40),
 	resource_memtotal int8,
 	resource_memused int8,
 	resource_swaptotal int8,
 	resource_swapused int8,
-	resource_hostname char(60),
+	resource_hostname varchar(60),
 	resource_load decimal(4,2),
 	resource_execdport int8,
 	resource_senddelay int8,
-	resource_capabilities char(255),
-	resource_lastgood char(10),
-	resource_state char(20),
-	resource_event char(20)
+	resource_capabilities varchar(255),
+	resource_lastgood varchar(10),
+	resource_state varchar(20),
+	resource_event varchar(20)
 );
 
 
 drop table kernel_info;
 create table kernel_info(
 	kernel_id int8  NOT NULL PRIMARY KEY,
-	kernel_name char(50),
-	kernel_version char(50),
-	kernel_capabilities char(255)
+	kernel_name varchar(50),
+	kernel_version varchar(50),
+	kernel_capabilities varchar(255)
 );
 
 
 drop table image_info;
 create table image_info(
 	image_id int8  NOT NULL PRIMARY KEY,
-	image_name char(50),
-	image_version char(30),
-	image_type char(20),
-	image_rootdevice char(255),
-	image_rootfstype char(10),
+	image_name varchar(50),
+	image_version varchar(30),
+	image_type varchar(20),
+	image_rootdevice varchar(255),
+	image_rootfstype varchar(10),
 	image_storageid int8,
-	image_deployment_parameter char(255),
+	image_deployment_parameter varchar(255),
 	image_isshared int8,
-	image_comment char(255),
-	image_capabilities char(255)
+	image_comment varchar(255),
+	image_capabilities varchar(255)
 );
 
 
 drop table appliance_info;
 create table appliance_info(
 	appliance_id int8 NOT NULL PRIMARY KEY,
-	appliance_name char(50),
+	appliance_name varchar(50),
 	appliance_kernelid int8,
 	appliance_imageid int8,
 	appliance_starttime int8,
 	appliance_stoptime int8,
 	appliance_cpunumber int8,
 	appliance_cpuspeed int8,
-	appliance_cpumodel char(40),
+	appliance_cpumodel varchar(40),
 	appliance_memtotal int8,
 	appliance_swaptotal int8,
-	appliance_capabilities char(255),
+	appliance_capabilities varchar(255),
 	appliance_cluster int8,
 	appliance_ssi int8,
 	appliance_resources int8,
 	appliance_highavailable int8,
 	appliance_virtual int8,
-	appliance_virtualization char(20),
+	appliance_virtualization varchar(20),
 	appliance_virtualization_host int8,
-	appliance_state char(20),
-	appliance_comment char(100),
-	appliance_event char(20)
+	appliance_state varchar(20),
+	appliance_comment varchar(100),
+	appliance_event varchar(20)
 );
 
 drop table event_info;
 create table event_info(
 	event_id int8 NOT NULL PRIMARY KEY,
-	event_name char(50),
-	event_time char(50),
+	event_name varchar(50),
+	event_time varchar(50),
 	event_priority int8,
-	event_source char(50),
-	event_description char(100),
-	event_comment char(100),
-	event_capabilities char(255),
+	event_source varchar(50),
+	event_description varchar(100),
+	event_comment varchar(100),
+	event_capabilities varchar(255),
 	event_status int8,
 	event_image_id int8,
 	event_resource_id int8
@@ -105,49 +105,49 @@ create table event_info(
 drop table user_info;
 create table user_info(
 	user_id int8 NOT NULL PRIMARY KEY,
-	user_name char(20),
-	user_password char(20),
-	user_gender char(1),
-	user_first_name char(50),
-	user_last_name char(50),
-	user_department char(50),
-	user_office char(50),
+	user_name varchar(20),
+	user_password varchar(20),
+	user_gender varchar(1),
+	user_first_name varchar(50),
+	user_last_name varchar(50),
+	user_department varchar(50),
+	user_office varchar(50),
 	user_role int8,
-	user_last_update_time char(50),
-	user_description char(255),
-	user_capabilities char(255),
-	user_state char(20)
+	user_last_update_time varchar(50),
+	user_description varchar(255),
+	user_capabilities varchar(255),
+	user_state varchar(20)
 );
 
 
 drop table role_info;
 create table role_info(
 	role_id int8 NOT NULL PRIMARY KEY,
-	role_name char(20)
+	role_name varchar(20)
 );
 
 
 drop table storage_info;
 create table storage_info(
 	storage_id int8 NOT NULL PRIMARY KEY,
-	storage_name char(20),
+	storage_name varchar(20),
 	storage_resource_id int8,
 	storage_type int8,
-	storage_comment char(255),
-	storage_capabilities char(255),
-	storage_state char(20)
+	storage_comment varchar(255),
+	storage_capabilities varchar(255),
+	storage_state varchar(20)
 );
 
 drop table resource_service;
 create table resource_service(
 	resource_id int8 NOT NULL PRIMARY KEY,
-	service char(50)
+	service varchar(50)
 );
 
 drop table image_service;
 create table image_service(
 	image_id int8 NOT NULL PRIMARY KEY,
-	service char(50)
+	service varchar(50)
 );
 
 drop table image_authentication_info;
@@ -162,20 +162,20 @@ drop table deployment_info;
 create table deployment_info(
 	deployment_id int8 NOT NULL PRIMARY KEY,
 	deployment_storagetype_id int8,
-	deployment_name char(50),
-	deployment_type char(50),
-	deployment_description char(50),
-	deployment_storagetype char(50),
-	deployment_storagedescription char(50),
-	deployment_mapping char(255)
+	deployment_name varchar(50),
+	deployment_type varchar(50),
+	deployment_description varchar(50),
+	deployment_storagetype varchar(50),
+	deployment_storagedescription varchar(50),
+	deployment_mapping varchar(255)
 );
 
 drop table virtualization_info;
 create table virtualization_info(
 	virtualization_id int8 NOT NULL PRIMARY KEY,
-	virtualization_name char(50),
-	virtualization_type char(20),
-	virtualization_mapping char(255)
+	virtualization_name varchar(50),
+	virtualization_type varchar(20),
+	virtualization_mapping varchar(255)
 );
 
 
@@ -203,4 +203,4 @@ grant all on resource_service to :openqrmdbuser;
 grant all on image_service to :openqrmdbuser;
 grant all on deployment_info to :openqrmdbuser;
 grant all on virtualization_info to :openqrmdbuser;
-grant all on storagetype_info to :openqrmdbuser;
+grant all on storage_info to :openqrmdbuser;
