b28aafcaacb662deca6e234e440079a939118a4e
mattr_sf <mattr_sf@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2009-07-29 13:40:53 +0000

    added copyright to files
    git-svn-id: http://openqrm.svn.sourceforge.net/svnroot/openqrm@1543 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

--- trunk/src/plugins/cloud/cloud-portal/web/mycloud-bottom.php
+++ trunk/src/plugins/cloud/cloud-portal/web/mycloud-bottom.php
@@ -1,3 +1,23 @@
+<?php
+
+/*
+  This file is part of openQRM.
+
+    openQRM is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2
+    as published by the Free Software Foundation.
+
+    openQRM is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with openQRM.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2009, Matthias Rechenburg <matt@openqrm.com>
+*/
+?>
 <br>
 &nbsp;&nbsp;&nbsp;<font size="2" face="Arial">This Cloud-Portal is brought to you by <a href="http://openqrm.sourceforge.net/" target="_BLANK">openQRM</a>, the open-source data-center management platform</font>
 <br>
