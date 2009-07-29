a3912a00c947da8c7b0e7078eebc0e5c49f50bf2
mattr_sf <mattr_sf@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2009-07-29 18:10:10 +0000

    added copyright to files
    git-svn-id: http://openqrm.svn.sourceforge.net/svnroot/openqrm@1558 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

--- trunk/src/plugins/lvm-storage/web/tpl/lvm-storage-about.tpl.php
+++ trunk/src/plugins/lvm-storage/web/tpl/lvm-storage-about.tpl.php
@@ -1,3 +1,22 @@
+<!--
+/*
+  This file is part of openQRM.
+
+    openQRM is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2
+    as published by the Free Software Foundation.
+
+    openQRM is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with openQRM.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2009, Matthias Rechenburg <matt@openqrm.com>
+*/
+-->
 <h1><img border=0 src="/openqrm/base/plugins/lvm-storage/img/plugin.png"> Lvm-storage plugin</h1>
 <br>
 The 'lvm-storage' plugin transforms a standard Linux-box into a rapid-fast-cloning storage-server
