d379d976992eea8f123f34f396ee988cdbf10a69
mattr_sf <mattr_sf@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2009-07-29 18:28:00 +0000

    added copyright to files
    git-svn-id: http://openqrm.svn.sourceforge.net/svnroot/openqrm@1561 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

--- trunk/src/plugins/vmware-esx/web/tpl/vmware-esx-about.tpl.php
+++ trunk/src/plugins/vmware-esx/web/tpl/vmware-esx-about.tpl.php
@@ -1,3 +1,22 @@
+<!--
+/*
+  This file is part of openQRM.
+
+    openQRM is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2
+    as published by the Free Software Foundation.
+
+    openQRM is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with openQRM.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2009, Matthias Rechenburg <matt@openqrm.com>
+*/
+-->
 <h1><img border=0 src="/openqrm/base/plugins/vmware-esx/img/plugin.png"> VMware-ESX plugin</h1>
 <strong>This plugin is tested with VMware ESX 3.5 - ESXi 4.0</strong>
 <br>
--- trunk/src/plugins/vmware-server/web/tpl/vmware-server-about.tpl.php
+++ trunk/src/plugins/vmware-server/web/tpl/vmware-server-about.tpl.php
@@ -1,3 +1,22 @@
+<!--
+/*
+  This file is part of openQRM.
+
+    openQRM is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2
+    as published by the Free Software Foundation.
+
+    openQRM is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with openQRM.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2009, Matthias Rechenburg <matt@openqrm.com>
+*/
+-->
 <h1><img border=0 src="/openqrm/base/plugins/vmware-server/img/plugin.png"> VMware Server 1 plugin</h1>
 <strong>This plugin is tested with VMware-server 1</strong>
 <br>
