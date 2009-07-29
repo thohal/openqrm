5ed76cd0fa8dcb924f606f74af7d349fc558db28
mattr_sf <mattr_sf@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2009-07-29 18:39:30 +0000

    added copyright to files
    git-svn-id: http://openqrm.svn.sourceforge.net/svnroot/openqrm@1563 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

--- trunk/src/plugins/zfs-storage/web/tpl/zfs-storage-luns.tpl.php
+++ trunk/src/plugins/zfs-storage/web/tpl/zfs-storage-luns.tpl.php
@@ -1,3 +1,22 @@
+<!--
+/*
+  This file is part of openQRM.
+
+    openQRM is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2
+    as published by the Free Software Foundation.
+
+    openQRM is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with openQRM.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2009, Matthias Rechenburg <matt@openqrm.com>
+*/
+-->
 <h1>iSCSI Luns of ZFS zpool {zpool_name} on storage {storage_name}</h1>
 {lun_table}
 <form action="{formaction}" method="GET">
