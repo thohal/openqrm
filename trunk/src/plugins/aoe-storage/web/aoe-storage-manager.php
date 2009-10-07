de310eb620ef63704c4a405976d5ede7ceef4329
mattr_sf <mattr_sf@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2009-10-07 16:21:37 +0000

    fixed paging/sorting in tables added to display lun size
    git-svn-id: http://openqrm.svn.sourceforge.net/svnroot/openqrm@1723 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

--- trunk/src/plugins/aoe-storage/web/aoe-storage-manager.php
+++ trunk/src/plugins/aoe-storage/web/aoe-storage-manager.php
@@ -345,6 +345,7 @@ function aoe_select_storage() {
 	global $OPENQRM_USER;
 	global $thisfile;
 	$table = new htmlobject_db_table('storage_id');
+    $table = new htmlobject_table_builder('storage_id', '', '', '', 'select');
 
 	$arHead = array();
 	$arHead['storage_state'] = array();
@@ -363,6 +364,7 @@ function aoe_select_storage() {
 
 	$arHead['storage_resource_id'] = array();
 	$arHead['storage_resource_id']['title'] ='Res.';
+	$arHead['storage_resource_id']['sortable'] = false;
 
 	$arHead['storage_resource_ip'] = array();
 	$arHead['storage_resource_ip']['title'] ='Ip';
@@ -375,10 +377,11 @@ function aoe_select_storage() {
 	$arHead['storage_comment'] = array();
 	$arHead['storage_comment']['title'] ='Comment';
 
-	$storage_count=0;
 	$arBody = array();
+    $t_deployment = new deployment();
+    $t_deployment->get_instance_by_type("aoe-deployment");
 	$storage_tmp = new storage();
-	$storage_array = $storage_tmp->display_overview($table->offset, $table->limit, $table->sort, $table->order);
+	$storage_array = $storage_tmp->display_overview_per_type($t_deployment->id, $table->offset, $table->limit, $table->sort, $table->order);
 	foreach ($storage_array as $index => $storage_db) {
 		$storage = new storage();
 		$storage->get_instance_by_id($storage_db["storage_id"]);
@@ -386,29 +389,25 @@ function aoe_select_storage() {
 		$storage_resource->get_instance_by_id($storage->resource_id);
 		$deployment = new deployment();
 		$deployment->get_instance_by_id($storage->type);
-		// is aoe storage ?
-		if ("$deployment->storagetype" == "aoe-storage") {
-			$storage_count++;
-			$resource_icon_default="/openqrm/base/img/resource.png";
-			$storage_icon="/openqrm/base/plugins/aoe-storage/img/storage.png";
-			$state_icon="/openqrm/base/img/$storage_resource->state.png";
-			if (!file_exists($_SERVER["DOCUMENT_ROOT"]."/".$state_icon)) {
-				$state_icon="/openqrm/base/img/unknown.png";
-			}
-			if (file_exists($_SERVER["DOCUMENT_ROOT"]."/".$storage_icon)) {
-				$resource_icon_default=$storage_icon;
-			}
-			$arBody[] = array(
-				'storage_state' => "<img src=$state_icon>",
-				'storage_icon' => "<img width=24 height=24 src=$resource_icon_default>",
-				'storage_id' => $storage->id,
-				'storage_name' => $storage->name,
-				'storage_resource_id' => $storage->resource_id,
-				'storage_resource_ip' => $storage_resource->ip,
-				'storage_type' => "$deployment->storagedescription",
-				'storage_comment' => $storage_resource->comment,
-			);
-		}
+        $resource_icon_default="/openqrm/base/img/resource.png";
+        $storage_icon="/openqrm/base/plugins/aoe-storage/img/storage.png";
+        $state_icon="/openqrm/base/img/$storage_resource->state.png";
+        if (!file_exists($_SERVER["DOCUMENT_ROOT"]."/".$state_icon)) {
+            $state_icon="/openqrm/base/img/unknown.png";
+        }
+        if (file_exists($_SERVER["DOCUMENT_ROOT"]."/".$storage_icon)) {
+            $resource_icon_default=$storage_icon;
+        }
+        $arBody[] = array(
+            'storage_state' => "<img src=$state_icon>",
+            'storage_icon' => "<img width=24 height=24 src=$resource_icon_default>",
+            'storage_id' => $storage->id,
+            'storage_name' => $storage->name,
+            'storage_resource_id' => $storage->resource_id,
+            'storage_resource_ip' => $storage_resource->ip,
+            'storage_type' => "$deployment->storagedescription",
+            'storage_comment' => $storage_resource->comment,
+        );
 	}
 	$table->id = 'Tabelle';
 	$table->css = 'htmlobject_table';
@@ -423,7 +422,7 @@ function aoe_select_storage() {
 		$table->bottom = array('select');
 		$table->identifier = 'storage_id';
 	}
-    $table->max = $storage_tmp->get_count();
+    $table->max = $storage_tmp->get_count_per_type($t_deployment->id);
 
    // set template
 	$t = new Template_PHPLIB();
@@ -456,15 +455,16 @@ function aoe_storage_display($aoe_storage_id) {
 
     $arHead['aoe_lun_icon'] = array();
 	$arHead['aoe_lun_icon']['title'] ='';
+	$arHead['aoe_lun_icon']['sortable'] = false;
 
 	$arHead['aoe_lun_name'] = array();
 	$arHead['aoe_lun_name']['title'] ='Name';
 
-	$arHead['aoe_lun'] = array();
-	$arHead['aoe_lun']['title'] ='Lun';
-
 	$arHead['aoe_lun_no'] = array();
-	$arHead['aoe_lun_no']['title'] ='Id.';
+	$arHead['aoe_lun_no']['title'] ='Lun';
+
+	$arHead['aoe_lun_size'] = array();
+	$arHead['aoe_lun_size']['title'] ='Size';
 
 	$arHead['aoe_lun_nic'] = array();
 	$arHead['aoe_lun_nic']['title'] ='Nic';
@@ -477,7 +477,7 @@ function aoe_storage_display($aoe_storage_id) {
 
 
 	$arBody = array();
-	$aoe_count=1;
+	$aoe_count=0;
 	$storage_icon="/openqrm/base/plugins/aoe-storage/img/storage.png";
 
 	$storage_export_list="storage/$storage_resource->id.aoe.stat";
@@ -485,53 +485,54 @@ function aoe_storage_display($aoe_storage_id) {
 		$storage_vg_content=file($storage_export_list);
 		foreach ($storage_vg_content as $index => $aoe) {
 			// find export name
-			if (!strstr($aoe, "#")) {
-                $aoe_line = trim($aoe);
-                $first_at_pos = strpos($aoe_line, "@");
-                $first_at_pos++;
-                $aoe_line_first_at_removed = substr($aoe_line, $first_at_pos, strlen($aoe_line)-$first_at_pos);
-                $second_at_pos = strpos($aoe_line_first_at_removed, "@");
-                $second_at_pos++;
-                $aoe_line_second_at_removed = substr($aoe_line_first_at_removed, $second_at_pos, strlen($aoe_line_first_at_removed)-$second_at_pos);
-                $third_at_pos = strpos($aoe_line_second_at_removed, "@");
-                $third_at_pos++;
-                $aoe_line_third_at_removed = substr($aoe_line_second_at_removed, $third_at_pos, strlen($aoe_line_second_at_removed)-$third_at_pos);
-                $fourth_at_pos = strpos($aoe_line_third_at_removed, "@");
-                $fourth_at_pos++;
-                $aoe_line_fourth_at_removed = substr($aoe_line_third_at_removed, $fourth_at_pos, strlen($aoe_line_third_at_removed)-$fourth_at_pos);
-                $fivth_at_pos = strpos($aoe_line_fourth_at_removed, "@");
-                $fivth_at_pos++;
-                $aoe_line_fivth_at_removed = substr($aoe_line_fourth_at_removed, $fivth_at_pos, strlen($aoe_line_fourth_at_removed)-$fivth_at_pos);
-                $sixth_at_pos = strpos($aoe_line_fivth_at_removed, "@");
-                $sixth_at_pos++;
-                $aoe_line_sixth_at_removed = substr($aoe_line_fivth_at_removed, $sixth_at_pos, strlen($aoe_line_fivth_at_removed)-$sixth_at_pos);
-                $seventh_at_pos = strpos($aoe_line_sixth_at_removed, "@");
-                $seventh_at_pos++;
+            $aoe_line = trim($aoe);
+            $first_at_pos = strpos($aoe_line, "@");
+            $first_at_pos++;
+            $aoe_line_first_at_removed = substr($aoe_line, $first_at_pos, strlen($aoe_line)-$first_at_pos);
+            $second_at_pos = strpos($aoe_line_first_at_removed, "@");
+            $second_at_pos++;
+            $aoe_line_second_at_removed = substr($aoe_line_first_at_removed, $second_at_pos, strlen($aoe_line_first_at_removed)-$second_at_pos);
+            $third_at_pos = strpos($aoe_line_second_at_removed, "@");
+            $third_at_pos++;
+            $aoe_line_third_at_removed = substr($aoe_line_second_at_removed, $third_at_pos, strlen($aoe_line_second_at_removed)-$third_at_pos);
+            $fourth_at_pos = strpos($aoe_line_third_at_removed, "@");
+            $fourth_at_pos++;
+            $aoe_line_fourth_at_removed = substr($aoe_line_third_at_removed, $fourth_at_pos, strlen($aoe_line_third_at_removed)-$fourth_at_pos);
+            $fivth_at_pos = strpos($aoe_line_fourth_at_removed, "@");
+            $fivth_at_pos++;
+            $aoe_line_fivth_at_removed = substr($aoe_line_fourth_at_removed, $fivth_at_pos, strlen($aoe_line_fourth_at_removed)-$fivth_at_pos);
+            $sixth_at_pos = strpos($aoe_line_fivth_at_removed, "@");
+            $sixth_at_pos++;
+            $aoe_line_sixth_at_removed = substr($aoe_line_fivth_at_removed, $sixth_at_pos, strlen($aoe_line_fivth_at_removed)-$sixth_at_pos);
+            $seventh_at_pos = strpos($aoe_line_sixth_at_removed, "@");
+            $seventh_at_pos++;
 
-                $aoe_lun_nic = trim(substr($aoe_line, 0, $first_at_pos-1));
-                $aoe_lun = trim(substr($aoe_line_first_at_removed, 0, $second_at_pos-1));
-                $aoe_lun_no = trim(substr($aoe_line_second_at_removed, 0, $third_at_pos-1));
-                $aoe_lun_name = basename(trim(substr($aoe_line_third_at_removed, 0, $fourth_at_pos-1)));
-                $aoe_lun_auth = trim(substr($aoe_line_fourth_at_removed, 0, $fivth_at_pos-1));
-                // build the snap-shot input
-                $aoe_lun_snap = "<form action=\"$thisfile\" method=\"GET\">";
-                $aoe_lun_snap .= "<input type='hidden' name='aoe_storage_id' value=$aoe_storage_id>";
-                $aoe_lun_snap .= "<input type='hidden' name='aoe_lun_name' value=$aoe_lun_name>";
-                $aoe_lun_snap .= "<input type='text' name='aoe_lun_snap_name' value='' size='10' maxlength='20'>";
-                $aoe_lun_snap .= "<input type='submit' name='action' value='snap'>";
-                $aoe_lun_snap .= "</form>";
+            $aoe_lun_nic = trim(substr($aoe_line, 0, $first_at_pos-1));
+            $aoe_lun = trim(substr($aoe_line_first_at_removed, 0, $second_at_pos-1));
+            $aoe_lun_no = trim(substr($aoe_line_second_at_removed, 0, $third_at_pos-1));
+            $aoe_lun_name = basename(trim(substr($aoe_line_third_at_removed, 0, $fourth_at_pos-1)));
+            $aoe_lun_auth = trim(substr($aoe_line_fourth_at_removed, 0, $fivth_at_pos-1));
+            // get the size from the config file
+            $aoe_lun_size = trim($aoe_line_fivth_at_removed);
+            $aoe_lun_size = str_replace("size=", "", $aoe_lun_size);
+            // build the snap-shot input
+            $aoe_lun_snap = "<form action=\"$thisfile\" method=\"GET\">";
+            $aoe_lun_snap .= "<input type='hidden' name='aoe_storage_id' value=$aoe_storage_id>";
+            $aoe_lun_snap .= "<input type='hidden' name='aoe_lun_name' value=$aoe_lun_name>";
+            $aoe_lun_snap .= "<input type='text' name='aoe_lun_snap_name' value='' size='10' maxlength='20'>";
+            $aoe_lun_snap .= "<input type='submit' name='action' value='snap'>";
+            $aoe_lun_snap .= "</form>";
 
-                $arBody[] = array(
-                    'aoe_lun_icon' => "<img width=24 height=24 src=$storage_icon><input type='hidden' name='aoe_storage_id' value=$aoe_storage_id>",
-                    'aoe_lun_name' => $aoe_lun_name,
-                    'aoe_lun' => $aoe_lun,
-                    'aoe_lun_no' => $aoe_lun_no,
-                    'aoe_lun_nic' => $aoe_lun_nic,
-                    'aoe_lun_auth' => $aoe_lun_auth,
-                    'aoe_lun_snap' => $aoe_lun_snap,
-                );
-                $aoe_lun_count++;
-			}
+            $arBody[] = array(
+                'aoe_lun_icon' => "<img width=24 height=24 src=$storage_icon><input type='hidden' name='aoe_storage_id' value=$aoe_storage_id>",
+                'aoe_lun_name' => $aoe_lun_name,
+                'aoe_lun_no' => $aoe_lun_no,
+                'aoe_lun_size' => $aoe_lun_size." MB",
+                'aoe_lun_nic' => $aoe_lun_nic,
+                'aoe_lun_auth' => $aoe_lun_auth,
+                'aoe_lun_snap' => $aoe_lun_snap,
+            );
+            $aoe_count++;
 		}
 	}
 
@@ -541,8 +542,8 @@ function aoe_storage_display($aoe_storage_id) {
 	$table->cellspacing = 0;
 	$table->cellpadding = 3;
 	$table->form_action = $thisfile;
-    $table->identifier_type = "radio";
-	$table->sort = '';
+    $table->identifier_type = "checkbox";
+	$table->autosort = true;
 	$table->head = $arHead;
 	$table->body = $arBody;
 	if ($OPENQRM_USER->role == "administrator") {
