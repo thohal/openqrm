5dc3a2be0b2a2e3fcbc6fc696f8880b4453598f8
mattr_sf <mattr_sf@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2009-10-12 12:50:50 +0000

    fixed table sorting + paging added html template
    git-svn-id: http://openqrm.svn.sourceforge.net/svnroot/openqrm@1751 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

--- trunk/src/plugins/collectd/web/collectd-manager.php
+++ trunk/src/plugins/collectd/web/collectd-manager.php
@@ -49,34 +49,40 @@ function collectd_select() {
 	global $OPENQRM_USER;
     global $RootDir;
 	global $thisfile;
-	$table = new htmlobject_db_table('collectd_id');
+    $table = new htmlobject_table_builder('appliance_id', '', '', '', 'select');
+
 
 	$arHead = array();
-	$arHead['collectd_state'] = array();
-	$arHead['collectd_state']['title'] ='';
+	$arHead['appliance_state'] = array();
+	$arHead['appliance_state']['title'] ='';
+	$arHead['appliance_state']['sortable'] = false;
 
-	$arHead['collectd_icon'] = array();
-	$arHead['collectd_icon']['title'] ='';
+	$arHead['appliance_icon'] = array();
+	$arHead['appliance_icon']['title'] ='';
+	$arHead['appliance_icon']['sortable'] = false;
 
-	$arHead['collectd_id'] = array();
-	$arHead['collectd_id']['title'] ='ID';
+	$arHead['appliance_id'] = array();
+	$arHead['appliance_id']['title'] ='ID';
 
-	$arHead['collectd_name'] = array();
-	$arHead['collectd_name']['title'] ='Name';
+	$arHead['appliance_name'] = array();
+	$arHead['appliance_name']['title'] ='Name';
 
-	$arHead['collectd_resource_id'] = array();
-	$arHead['collectd_resource_id']['title'] ='Res.ID';
+	$arHead['appliance_resource_id'] = array();
+	$arHead['appliance_resource_id']['title'] ='Res.ID';
+	$arHead['appliance_resource_id']['sortable'] = false;
 
-	$arHead['collectd_resource_ip'] = array();
-	$arHead['collectd_resource_ip']['title'] ='Ip';
+	$arHead['appliance_resource_ip'] = array();
+	$arHead['appliance_resource_ip']['title'] ='Ip';
+	$arHead['appliance_resource_ip']['sortable'] = false;
 
-	$arHead['collectd_graph'] = array();
-	$arHead['collectd_graph']['title'] ='Graphs';
+	$arHead['appliance_graph'] = array();
+	$arHead['appliance_graph']['title'] ='Graphs';
+	$arHead['appliance_graph']['sortable'] = false;
 
 	$collectd_count=0;
 	$arBody = array();
 	$collectd_tmp = new appliance();
-	$collectd_array = $collectd_tmp->display_overview(0, 100, 'appliance_id', 'ASC');
+	$collectd_array = $collectd_tmp->display_overview($table->offset, $table->limit, $table->sort, $table->order);
 
 	foreach ($collectd_array as $index => $collectd_db) {
 		$collectd_app = new appliance();
@@ -108,13 +114,13 @@ function collectd_select() {
 			$state_icon=$active_state_icon;
     		$collectd_count++;
             $arBody[] = array(
-                'collectd_state' => "<img src=$state_icon>",
-                'collectd_icon' => "<img width=24 height=24 src=$resource_icon_default>",
-                'collectd_id' => $collectd_db["appliance_id"],
-                'collectd_name' => $collectd_db["appliance_name"],
-                'collectd_resource_id' => $collectd_resource->id,
-                'collectd_resource_ip' => $collectd_resource->ip,
-                'collectd_graph' => $collectd_graph,
+                'appliance_state' => "<img src=$state_icon>",
+                'appliance_icon' => "<img width=24 height=24 src=$resource_icon_default>",
+                'appliance_id' => $collectd_db["appliance_id"],
+                'appliance_name' => $collectd_db["appliance_name"],
+                'appliance_resource_id' => $collectd_resource->id,
+                'appliance_resource_ip' => $collectd_resource->ip,
+                'appliance_graph' => $collectd_graph,
             );
 		}
 	}
@@ -128,8 +134,16 @@ function collectd_select() {
 	$table->identifier_type = "radio";
 	$table->head = $arHead;
 	$table->body = $arBody;
-	$table->max = $collectd_count;
-	return $disp.$table->get_string();
+    $table->max = $collectd_tmp->get_count();
+    // set template
+	$t = new Template_PHPLIB();
+	$t->debug = false;
+	$t->setFile('tplfile', './tpl/' . 'collectd-select.tpl.php');
+	$t->setVar(array(
+        'collectd_table' => $table->get_string(),
+	));
+	$disp =  $t->parse('out', 'tplfile');
+	return $disp;
 }
 
 
