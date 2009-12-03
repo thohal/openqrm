449806f041870b867f547b27ce4167abc48fe2b3
infernix <infernix@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2009-12-03 20:40:26 +0000

    Add a resource "remove" hook that deletes the pxe config file when a kvm vm resource is deleted.
    git-svn-id: http://openqrm.svn.sourceforge.net/svnroot/openqrm@1877 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

---
+++ trunk/src/plugins/kvm/web/openqrm-kvm-resource-hook.php
@@ -0,0 +1,59 @@
+<?php
+/*
+  This file is part of openQRM.
+
+    openQRM is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2
+    as published by the Free Software Foundation.
+
+    openQRM is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with openQRM.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2009, Matthias Rechenburg <matt@openqrm.com>
+*/
+
+
+// error_reporting(E_ALL);
+
+$RootDir = $_SERVER["DOCUMENT_ROOT"].'/openqrm/base/';
+require_once "$RootDir/include/user.inc.php";
+require_once "$RootDir/class/event.class.php";
+require_once "$RootDir/class/resource.class.php";
+require_once "$RootDir/class/openqrm_server.class.php";
+require_once "$RootDir/include/openqrm-server-config.php";
+global $OPENQRM_SERVER_BASE_DIR;
+global $OPENQRM_EXEC_PORT;
+global $OPENQRM_SERVER_IP_ADDRESS;
+$event = new event();
+global $event;
+
+
+
+function openqrm_kvm_resource($cmd, $resource_fields) {
+	global $event;
+	global $OPENQRM_SERVER_BASE_DIR;
+	global $OPENQRM_SERVER_IP_ADDRESS;
+	global $OPENQRM_EXEC_PORT;
+	$resource_id=$resource_fields["resource_id"];
+	$resource_ip=$resource_fields["resource_ip"];
+	$resource_mac=$resource_fields["resource_mac"];
+	$event->log("openqrm_new_resource", $_SERVER['REQUEST_TIME'], 5, "openqrm-kvm-resource-hook.php", "Handling $cmd event $resource_id/$resource_ip/$resource_mac", "", "", 0, 0, $resource_id);
+	switch($cmd) {
+		case "remove":
+			// we remove the pxe config file; the database removal is handled in the resource class
+			$openqrm_server = new openqrm_server();
+			$openqrm_server->send_command("openqrm_remove_resource $resource_id $resource_mac");
+			break;
+	}
+}
+
+
+
+?>
+
+
